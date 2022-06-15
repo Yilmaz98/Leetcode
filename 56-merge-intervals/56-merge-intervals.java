b4c66f597fbd4183bbef454c57cb905778fc5f00
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-06-14 22:22:23 -0700

    Time: 18 ms (18.77%), Space: 55.3 MB (44.99%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,27 +1,26 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-    
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+       Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        
        Stack<int[]> st = new Stack<>();
-       st.push(intervals[0]);
-           
-       for(int i=1;i<intervals.length;i++) {
-           int[] prev = st.peek();
-           if(!st.isEmpty() && prev[1] >= intervals[i][0]) {
+        st.push(intervals[0]);
+        
+        for(int i=1;i<intervals.length;i++) {
+            int[] prev = st.peek();
+            
+            if(prev[1] >= intervals[i][0]) {
                 st.pop();
-                int[] newInterval = new int[2];
-                newInterval[0] = Math.min(prev[0], intervals[i][0]);
-                newInterval[1] = Math.max(intervals[i][1], prev[1]);
-                st.push(newInterval);
-           } 
-           else {
-               st.push(intervals[i]);
-           }
-       }
+                int[] newArr= new int[2];
+                newArr[0]= prev[0];
+                newArr[1] = Math.max(intervals[i][1], prev[1]);
+                st.push(newArr);
+            } else
+                st.push(intervals[i]);
+        }
         
-       int[][] ans = new int[st.size()][2];
+        int[][] ans = new int[st.size()][2];
         
-        for(int i=ans.length-1;i>=0;i--) {
+        for(int i= ans.length - 1;i>=0;i--) {
             ans[i] = st.pop();
         }
         
