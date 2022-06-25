99066e7b02980cbf6ae456779f17b630f9916664
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-06-25 15:34:48 -0700

    Time: 2 ms (30.69%), Space: 53.9 MB (46.45%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -1,9 +1,13 @@
 class Solution {
     public boolean checkPossibility(int[] N) {
-        for (int i = 1, err = 0; i < N.length; i++)
-            if (N[i] < N[i-1])
-                if (err++ > 0 || (i > 1 && i < N.length - 1 && N[i-2] > N[i] && N[i+1] < N[i-1]))
+        int err = 0;
+        for (int i = 1; i < N.length; i++) {
+            if (N[i] < N[i-1]) {
+                if (err > 0 || (i > 1 && i < N.length - 1 && N[i-2] > N[i] && N[i-1] > N[i+1]))
                     return false;
+                err = 1;
+                }
+        }
         return true;
     }
 }
