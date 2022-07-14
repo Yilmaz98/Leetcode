4c00b705706efac4050ecc2d66f762134aed959f
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-07-14 10:20:45 -0700

    Time: 2 ms (78.80%), Space: 52.4 MB (53.79%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+            int sum = 0;
+        for(int num : nums) sum += num;
+        
+        int prefixSum = 0; // prefix sum to the left of index i
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(prefixSum * 2 == sum - nums[i]) return i;
+            prefixSum += nums[i];
+        }
+        return -1;
+    }
+}
