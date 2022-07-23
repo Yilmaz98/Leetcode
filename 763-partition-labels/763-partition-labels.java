ca197f7ee7a640d27eadab0fe6e0fdf1b1113eb0
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-07-22 22:01:12 -0700

    Time: 6 ms (65.09%), Space: 42.5 MB (72.73%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+             int[] last = new int[26];
+        for (int i = 0; i < s.length(); ++i)
+            last[s.charAt(i) - 'a'] = i;
+        
+        int boundary = 0, cnt = 0;
+        List<Integer> ans = new ArrayList();
+        for (int i = 0; i < s.length(); ++i) {
+            // update boundary and cnt along the way
+            char curChar = s.charAt(i);
+            boundary = Math.max(boundary, last[curChar - 'a']);
+            cnt++;
+            // if we reach the boundary, we collect result and reset count to 0
+            if (i == boundary) {
+                ans.add(cnt);
+                cnt = 0;
+            }
+        }
+        return ans;
+    }
+}
