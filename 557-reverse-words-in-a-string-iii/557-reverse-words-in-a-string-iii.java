9f501c41e347a7e03fac85e7ad064e2af27c8eec
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-07-28 00:04:23 -0700

    Time: 103 ms (16.17%), Space: 117.9 MB (11.86%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -2,11 +2,13 @@ class Solution {
     public String reverseWords(String s) {
         String words[] = s.split(" ");
         String result = "";
+        StringBuilder sb = new StringBuilder();
         for(int i=0;i<words.length;i++) {
-            StringBuilder sb = new StringBuilder(words[i]);
+            sb.append(words[i]);
             result += sb.reverse().toString();
             if(i!=words.length - 1)
                 result +=" ";
+            sb.setLength(0);
         }
         
         return result;
