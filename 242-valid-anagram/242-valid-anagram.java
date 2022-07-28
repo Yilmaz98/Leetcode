1e1c8b230370a6973008d5c00b65f45f550490c4
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-07-28 00:09:30 -0700

    Time: 4 ms (85.70%), Space: 44 MB (51.50%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         int[] count = new int[26];
-        Arrays.fill(count,0);
         for(int i=0;i<s.length();i++) {
             count[s.charAt(i)-'a'] ++;
         }
