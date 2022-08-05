3bffb71aa9a7d20bbd93fcee0dc62f1dac308eb8
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-08-04 18:27:58 -0700

    Time: 1 ms (58.54%), Space: 41 MB (63.41%) - LeetHub

--- 780-reaching-points/780-reaching-points.java
+++ 780-reaching-points/780-reaching-points.java
@@ -1,9 +1,21 @@
 class Solution {
     public boolean reachingPoints(int sx, int sy, int tx, int ty) {
-           while (sx < tx && sy < ty)
-            if (tx < ty) ty %= tx;
-            else tx %= ty;
-        return sx == tx && sy <= ty && (ty - sy) % sx == 0 ||
-               sy == ty && sx <= tx && (tx - sx) % sy == 0;
+        if(sx == tx && sy == ty)
+            return true;
+        
+        while(tx > sx && ty > sy) {
+              if(tx < ty)
+                ty %= tx;
+            else
+                tx %= ty; 
+        }
+        
+        if(sx == tx && sy < ty && (ty - sy) % sx ==0)
+            return true;
+        
+        if(sy == ty && sx < tx && (tx - sx) % sy ==0)
+            return true;
+    
+        return false;
     }
 }
