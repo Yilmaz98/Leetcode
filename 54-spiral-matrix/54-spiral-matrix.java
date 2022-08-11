bdde8f49aec133ed361317407a0ed5122376e768
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-08-10 23:19:15 -0700

    Time: 0 ms (100.00%), Space: 41.9 MB (70.92%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,30 +1,25 @@
 class Solution {
-    List<Integer> res = new ArrayList<>();
-    public void dfs(int[][] matrix, int i, int j, boolean[][] visited, int m,int n,  boolean up) {
-        if(i<0 || i>=m || j<0 || j>=n || visited[i][j] || res.size() == m*n) {
-            return;
-        }
-        
-        visited[i][j] = true;
-        res.add(matrix[i][j]);
-        
-        if(up) {
-            dfs(matrix, i-1, j, visited, m,n, true);
-        }
-        
-        dfs(matrix, i, j+1, visited,m,n, false);
-        dfs(matrix, i+1, j, visited, m,n, false);
-        dfs(matrix, i, j-1, visited, m,n,false);
-        dfs(matrix, i-1, j, visited, m,n, true);
-        visited[i][j] = false;
-    }
-    
+    List<Integer> result = new ArrayList<>();
     public List<Integer> spiralOrder(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        
-        boolean visited[][] = new boolean[m][n];
-        dfs(matrix,0,0,visited, m,n, false);
-        return res;
+        boolean[][] visited = new boolean[m][n];
+        dfs(matrix,0,0,m,n,false, visited);
+        return result;
+    }
+    
+    public void dfs(int[][] matrix,int i, int j, int m, int n, boolean up, boolean[][] visited) {
+        if(i<0 || i>=m || j<0 || j>=n || visited[i][j] == true) {
+            return;
+        }
+        visited[i][j] = true;
+        result.add(matrix[i][j]);
+        if(up) {
+            dfs(matrix,i-1,j,m,n,true,visited); 
+        }
+        dfs(matrix,i,j+1,m,n,false,visited);
+        dfs(matrix,i+1,j,m,n,false,visited);
+        dfs(matrix,i,j-1,m,n,false,visited);
+        dfs(matrix,i-1,j,m,n,true,visited);
     }
 }
