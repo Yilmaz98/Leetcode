9a782ae9d03a47ab68866bc5dc947b2744887908
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-08-11 02:02:48 -0700

    Time: 1 ms (100.00%), Space: 51.5 MB (94.31%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -4,13 +4,13 @@ class Solution {
         int maxSum = Integer.MIN_VALUE;
         
         for(int i=0;i<nums.length;i++) {
-            sum += nums[i];
-            maxSum = Math.max(maxSum,sum);
-            if(sum < 0) {
+            sum +=nums[i];
+            maxSum = Math.max(sum,maxSum);
+            if(sum<0)
                 sum = 0;
-            }
-            
-        }        
+
+        }
+        
         return maxSum;
     }
 }
