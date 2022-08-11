d8e785d19e41a45579f311001fbbb920b94506ec
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-08-10 21:55:46 -0700

    Time: 0 ms (100.00%), Space: 38.9 MB (94.20%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.java
+++ 371-sum-of-two-integers/371-sum-of-two-integers.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int getSum(int a, int b) {        
-        return b == 0? a: getSum(a^b, (a&b) << 1);
+    public int getSum(int a, int b) {
+        return b == 0 ? a: getSum(a ^ b, (a&b) << 1);
     }
 }
