17a4c1005d281942f9dea78acb16cc75bd727070
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-08-13 09:50:05 -0700

    Time: 0 ms (100.00%), Space: 39.5 MB (97.94%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -7,7 +7,7 @@ class Solution {
         int dp[] = new int[n];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0],nums[1]);
-        for(int i=2;i<nums.length;i++) {
+        for(int i=2;i<n;i++) {
             dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
         }
         
