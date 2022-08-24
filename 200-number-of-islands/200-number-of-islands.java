beafed28a57639ddcf1caf71196d7968618d8df4
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-08-24 18:42:01 -0400

    Time: 3 ms (97.74%), Space: 58.4 MB (9.61%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,51 +1,33 @@
 class Solution {
-    int x[] = {-1,0,0,1};
-    int y[] = {0,-1,1,0};
-    public class Node {
-        int x;
-        int y;
-        Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    
-    public boolean isSafe(int r, int c, char[][] grid,int m, int n) {
-        if(r<0 || r>=m || c<0 || c>=n || grid[r][c] == '0') {
-            return false;
-        }
-        return true;
-    }
-    
-    public void bfs(char[][] grid, int i, int j, int m, int n) {
-        Queue<Node> qu = new LinkedList<>();
-        qu.offer(new Node(i,j));
-        while(!qu.isEmpty()) {
-            Node nu = qu.poll();
-            for(int k=0;k<4;k++) {
-                int nr = nu.x + x[k]; 
-                int nc = nu.y + y[k]; 
-                if(isSafe(nr,nc,grid, m,n)) {
-                    grid[nr][nc] = '0';
-                    qu.offer(new Node(nr,nc));
-                }
-            }
-        }
-    }
-    
     public int numIslands(char[][] grid) {
         int m = grid.length;
-        int n = grid[0].length;        
+        int n = grid[0].length;
         int count = 0;
         
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
                 if(grid[i][j] == '1') {
-                    bfs(grid,i,j,m,n);
+                    dfs(i,j,m,n,grid);
                     count++;
                 }
             }
         }
+        
         return count;
+    }
+    
+    
+    public void dfs(int i, int j, int m, int n,  char[][] grid) {
+        if(i<0 || i>= m || j< 0 || j>=n || grid[i][j] == '0')
+            return;
+        
+        grid[i][j] = '0';
+        
+        dfs(i+1, j, m,n,grid);
+        dfs(i-1, j, m,n,grid);
+        dfs(i,j+1, m,n,grid);
+        dfs(i,j-1,m,n,grid);
+        
+        return; 
     }
 }
