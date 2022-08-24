fa1a66784de232295e477ffba6006c62c76a7b84
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-08-24 01:43:30 -0400

    Time: 12 ms (56.94%), Space: 49.7 MB (24.13%) - LeetHub

--- 910-smallest-range-ii/910-smallest-range-ii.java
+++ 910-smallest-range-ii/910-smallest-range-ii.java
@@ -3,11 +3,14 @@ class Solution {
             int N = A.length;
         Arrays.sort(A);
         int ans = A[N-1] - A[0];
+        
+        int highBound = A[N-1] - K;
+        int lowBound = A[0] + K;
 
         for (int i = 0; i < A.length - 1; ++i) {
             int a = A[i], b = A[i+1];
-            int high = Math.max(A[N-1] - K, a + K);
-            int low = Math.min(A[0] + K, b - K);
+            int high = Math.max(highBound, a + K);
+            int low = Math.min(lowBound, b - K);
             ans = Math.min(ans, high - low);
         }
         return ans;
