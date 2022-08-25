55100d36ac8a17c0ec17dfce6d4cb2028ceed97a
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-08-25 01:52:40 -0400

    Time: 46 ms (49.84%), Space: 54.2 MB (72.72%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,29 +1,15 @@
 class MyCalendar {
-    TreeMap<Integer, Integer> m = new TreeMap<>();
-    
-    public MyCalendar() {
 
+    public MyCalendar() {
+        
     }
-    
-    public boolean book(int start, int end) {
-        m.put(start, m.getOrDefault(start,0) + 1);
-        m.put(end, m.getOrDefault(end,0) - 1);
-        
-        int sum = 0;
-        
-        for(Integer i: m.keySet()) {
-            sum += m.get(i);   
-            if(sum > 1) {
-                m.put(start,m.get(start)-1);
-                if(m.get(start) == 0)
-                    m.remove(start);
-                m.put(end,m.get(end)+1);
-                 if(m.get(end) == 0)
-                    m.remove(end);
-                return false; 
-            }      
-        }
-        
+        TreeMap<Integer, Integer> books = new TreeMap<>();
+
+    public boolean book(int s, int e) {
+        java.util.Map.Entry<Integer, Integer> floor = books.floorEntry(s), ceiling = books.ceilingEntry(s);
+        if (floor != null && s < floor.getValue()) return false; // (s, e) start within floor
+        if (ceiling != null && ceiling.getKey() < e) return false; // ceiling start within (s, e)
+        books.put(s, e);
         return true;
     }
 }
