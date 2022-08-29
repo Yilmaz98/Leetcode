39d62fa226bf4e3a6e307e1f217d4f38da422035
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-08-28 23:58:18 -0400

    Time: 6 ms (54.48%), Space: 41.9 MB (77.43%) - LeetHub

---
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int shortestWay(String source, String target) {
+        int ans = 0;
+        
+        String remaining = target;   
+        while(remaining.length() > 0) {
+ 
+            int i = 0;
+            int j = 0;    
+            StringBuilder subsequence = new StringBuilder();
+            while(i< source.length() && j<remaining.length()) {
+                if(source.charAt(i++) == remaining.charAt(j))
+                    subsequence.append(remaining.charAt(j++));
+            }
+            
+            if(subsequence.length() == 0)
+                return -1;
+                
+            ans++;
+            remaining = remaining.substring(subsequence.length());
+        }
+        
+        
+        return ans;
+    }
+}
