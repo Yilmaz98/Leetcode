1325fcb81765538326174d2472f8b6ef7adc285c
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-08-31 23:55:34 -0400

    Time: 3 ms (76.91%), Space: 59.4 MB (69.59%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -14,47 +14,17 @@
  * }
  */
 class Solution {
-    public class SpecialNode {
-        TreeNode node;
-        boolean isMax;
-        int currentMax;
-        
-        SpecialNode(TreeNode node, boolean isMax, int currentMax) {
-            this.node = node;
-            this.isMax = isMax;
-            this.currentMax = currentMax;
-        }
+    public int goodNodes(TreeNode root) {
+        return count(root,root.val); 
     }
     
-    public int goodNodes(TreeNode root) {
-        int goodNodes = 0;
-        Queue<SpecialNode> q = new LinkedList<>();
-        q.add(new SpecialNode(root,true,root.val));
-        
-        while(!q.isEmpty()) {
-            SpecialNode curr = q.poll();
-            
-            if(curr.isMax == true) {
-                goodNodes++;
-            }
-            
-            if(curr.node.left != null) {
-                if(curr.node.left.val >= curr.currentMax) {      
-                    q.add(new SpecialNode(curr.node.left,true,curr.node.left.val));  
-                } else {
-                    q.add(new SpecialNode(curr.node.left, false, curr.currentMax));
-                }
-            }
-            
-            if(curr.node.right != null) {
-            if(curr.node.right.val >= curr.currentMax) {
-                    q.add(new SpecialNode(curr.node.right,true,curr.node.right.val));  
-                } else {
-                    q.add(new SpecialNode(curr.node.right, false, curr.currentMax));
-                }
-            }
+    public int count(TreeNode root, int val) {
+        if(root == null) {
+            return 0;
         }
-        
-        return goodNodes;
+        int max = Math.max(val, root.val);
+    
+        return (root.val >= val ? 1: 0) + count(root.left, max) + count(root.right,max);
     }
+     
 }
