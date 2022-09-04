675cf5e37f4e5e24253a0fa5a9431701732c10fd
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-09-03 23:22:37 -0400

    Time: 245 ms (22.87%), Space: 68.4 MB (86.63%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int dfs(Map<Integer,List<Integer>> m, int headID, int[] informTime) {       
+        int max = 0;
+        if(!m.containsKey(headID)) {
+            return informTime[headID];
+        }
+        
+        for(int i=0;i<m.get(headID).size();i++) {
+            max = Math.max(max, informTime[headID] +  dfs(m,m.get(headID).get(i),informTime));
+        }    
+        return max;  
+    } 
+    
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime)     {
+        Map<Integer, List<Integer>> m = new HashMap<>();
+        
+        for(int i=0;i<manager.length;i++) {
+            if(manager[i]<0)
+                continue;
+            if(!m.containsKey(manager[i])) {
+                m.put(manager[i], new ArrayList<>());
+            }
+            
+            m.get(manager[i]).add(i);
+        }
+        System.out.println(m);
+        
+        return dfs(m, headID, informTime);
+    }
+}
