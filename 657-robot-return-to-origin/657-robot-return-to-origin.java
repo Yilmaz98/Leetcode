24ad5382d31564bb0e1189899631feb8fa57c11f
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-09-11 02:23:37 -0400

    Time: 21 ms (7.34%), Space: 44.5 MB (78.85%) - LeetHub

--- 657-robot-return-to-origin/657-robot-return-to-origin.java
+++ 657-robot-return-to-origin/657-robot-return-to-origin.java
@@ -1,33 +1,26 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int[] xdir = {-1,1,0,0};
-        int[] ydir = {0,0,1,-1};
-        
-        int[] curr = new int[]{0,0};
+        int ud = 0;
+        int lr = 0;
         
         for(int i=0;i<moves.length();i++) {
             if(moves.charAt(i) == 'L') {
-               
-                curr[0] += xdir[0];
-                curr[1] += ydir[0]; 
+               lr++;
             }
             
             if(moves.charAt(i) == 'R') {
-               curr[0] += xdir[1];
-                curr[1] += ydir[1]; 
+               lr--;
             }
                 
             if(moves.charAt(i) == 'U') {
-               curr[0] += xdir[2];
-                curr[1] += ydir[2]; 
+               ud++;
             }
             
             if(moves.charAt(i) == 'D') {
-               curr[0] += xdir[3];
-                curr[1] += ydir[3]; 
+               ud--;
             }
         }
         
-        return curr[0] == 0 && curr[1] == 0;
+        return lr == 0 && ud == 0;
     }
 }
