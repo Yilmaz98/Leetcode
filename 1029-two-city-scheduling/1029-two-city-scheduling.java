f85fc842849bfad5f8bfe0b663974c2c28244f8c
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-09-13 00:55:44 -0400

    Time: 4 ms (27.96%), Space: 42.6 MB (25.46%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+       int cityCost = 0;
+        
+        for(int[] cost : costs) {
+            cityCost += cost[0];
+        }
+        
+        int n = costs.length;
+        int[] refund = new int[n];
+        
+        for(int i=0;i<n;i++) {
+            refund[i] = costs[i][1] - costs[i][0];
+        }
+        
+        Arrays.sort(refund);
+        
+        for(int i=0;i<n/2;i++) {
+            cityCost += refund[i];
+        }
+        
+        
+        return cityCost;
+        
+    }
+}
