04824f1b7568e4552226bffee46589db1487bb8c
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-09-22 16:24:47 -0400

    Time: 0 ms (100.00%), Space: 39.8 MB (97.05%) - LeetHub

--- 1037-valid-boomerang/1037-valid-boomerang.java
+++ 1037-valid-boomerang/1037-valid-boomerang.java
@@ -1,5 +1,8 @@
 class Solution {
       public boolean isBoomerang(int[][] p) {
-        return (p[0][0] - p[1][0]) * (p[0][1] - p[2][1]) != (p[0][0] - p[2][0]) * (p[0][1] - p[1][1]);
+        int firstSlope =  (p[1][1] - p[0][1]) * (p[2][0] - p[1][0]);
+        int secondSlope = (p[2][1] - p[1][1]) * (p[1][0] - p[0][0]);
+          
+        return firstSlope != secondSlope;
     }
 }
