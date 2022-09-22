04add93e200688c8fedeacc7a37365334d9d596b
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-09-22 02:22:17 -0400

    Time: 3 ms (61.80%), Space: 41.8 MB (82.45%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,22 +1,18 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        Map<Character,Integer> m = new HashMap<>();
-        
-        for(Character c : s.toCharArray()) {
-            m.put(c, m.getOrDefault(c,0)+1);
+        // Initialize ch with 0, because 0 ^ X = X
+        // 0 when XORed with any bit would not change the bits value.
+        char ch = 0;
+
+        // XOR all the characters of both s and t.
+        for (int i = 0; i < s.length(); i += 1) {
+            ch ^= s.charAt(i);
         }
-        
-        for(Character c : t.toCharArray()) {
-           if(m.containsKey(c)) {
-               m.put(c, m.getOrDefault(c,0)-1);
-               if(m.get(c) == 0)
-                   m.remove(c);
-           } else {
-               m.put(c,m.getOrDefault(c,0)+1);
-           }
+        for (int i = 0; i < t.length(); i += 1) {
+            ch ^= t.charAt(i);
         }
-        
-        
-        return m.keySet().iterator().next();
+
+        // What is left after XORing everything is the difference.
+        return ch;
     }
 }
