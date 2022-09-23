68c805cc2f854706a3d17db2fac9e5b724eb476e
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-09-23 02:01:18 -0400

    Time: 72 ms (36.77%), Space: 41.3 MB (53.23%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.java
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.java
@@ -7,12 +7,19 @@ class Solution {
     int drop(int floors, int eggs, int[][] dp) {
         if (eggs == 1 || floors <= 1)
             return floors;
+        
+        
         if (dp[floors][eggs] > 0)
             return dp[floors][eggs];
+        
+        
         int min = Integer.MAX_VALUE;
+
         for (int f = 1; f <= floors; f++)
             min = Math.min(min, 1 + Math.max(drop(f - 1, eggs - 1, dp), drop(floors - f, eggs, dp)));
+        
         dp[floors][eggs] = min;
-        return min;
+        
+        return  dp[floors][eggs];
     }
 }
