803c0171cb6b6323cca2b1d67794fc34c09ee953
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-09-23 23:28:14 -0400

    Time: 18 ms (5.14%), Space: 43.8 MB (23.56%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -3,7 +3,10 @@ class Solution {
         int low = 0;
         int high = arr.length;
         while (low < high) {
+             System.out.println("Low:" + low);
+             System.out.println("High:" + high);
             int mid = low + (high - low) / 2;
+            System.out.println("Mid:" + mid);
             if (arr[mid] - (mid + 1) >= k) 
                 high = mid;  
             else 
