5033893c25096717befc45f6ab7897b702fd5118
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-09-23 20:24:53 -0400

    Time: 200 ms (48.06%), Space: 141.3 MB (6.99%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.java
+++ 2013-detect-squares/2013-detect-squares.java
@@ -1,21 +1,32 @@
 class DetectSquares {
-    
-        int[][] cntPoints = new int[1001][1001];
+    int[][] count = new int[1001][1001];
     List<int[]> points = new ArrayList<>();
-
-    public void add(int[] p) {
-        cntPoints[p[0]][p[1]] += 1;
-        points.add(p);
+    
+    
+    public DetectSquares() {
+        
     }
-
-    public int count(int[] p1) {
-        int x1 = p1[0], y1 = p1[1], ans = 0;
-        for (int[] p3 : points) {
-            int x3 = p3[0], y3 = p3[1];
-            if (Math.abs(x1-x3) == 0 || Math.abs(x1-x3) != Math.abs(y1-y3))
-                continue; // Skip empty square or invalid square point!
-            ans += cntPoints[x1][y3] * cntPoints[x3][y1];
+    
+    public void add(int[] point) {
+        count[point[0]][point[1]]++;
+        points.add(point);
+    }
+    
+    public int count(int[] point) {
+        int x1 = point[0];
+        int y1 = point[1];
+        int ans = 0;
+        
+        for(int[] p : points) {
+            int x3 = p[0];
+            int y3 = p[1];
+            
+            if(Math.abs(x1 - x3) == 0 || Math.abs(x1 - x3) != Math.abs(y1-y3))
+                continue;
+            
+            ans += count[x1][y3] * count[x3][y1]; 
         }
+        
         return ans;
     }
 }
