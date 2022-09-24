23b78fd933a6b6219b8dcd039f083caf23392c8d
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-09-24 17:07:58 -0400

    Time: 253 ms (10.52%), Space: 71.2 MB (22.94%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,30 +1,52 @@
 class Solution {
-    int[] DIR = new int[]{0, 1, 0, -1, 0};
+    int[] dirX = {-1,0,0,1};
+    int[] dirY = {0,1,-1,0};
+    
     public int minimumEffortPath(int[][] heights) {
-        int m = heights.length, n = heights[0].length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[2] - b[2]);
+        
+        pq.add(new int[]{0,0,0});
+        
+        
+        int m = heights.length;
+        int n = heights[0].length;
+        
+        boolean[][] visited = new boolean[m][n];
+        
         int[][] dist = new int[m][n];
-        for (int i = 0; i < m; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
+        dist[0][0] = heights[0][0];
         
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-        minHeap.offer(new int[]{0, 0, 0}); // distance, row, col
-        dist[0][0] = 0;
+        for(int i=0;i<m;i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        }
         
-        while (!minHeap.isEmpty()) {
-            int[] top = minHeap.poll();
-            int d = top[0], r = top[1], c = top[2];
-            if (d > dist[r][c]) continue; // this is an outdated version -> skip it
-            if (r == m - 1 && c == n - 1) return d; // Reach to bottom right
-            for (int i = 0; i < 4; i++) {
-                int nr = r + DIR[i], nc = c + DIR[i + 1];
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
-                    int newDist = Math.max(d, Math.abs(heights[nr][nc] - heights[r][c]));
-                    if (dist[nr][nc] > newDist) {
-                        dist[nr][nc] = newDist;
-                        minHeap.offer(new int[]{dist[nr][nc], nr, nc});
+        while(!pq.isEmpty()) {
+            int size = pq.size();
+            
+            for(int i=0;i<size;i++) {
+                int[] curr = pq.poll();
+                
+                visited[curr[0]][curr[1]] = true;
+                
+               
+                
+                for(int j=0; j<4;j++) {
+                    int newX = curr[0] + dirX[j];
+                    int newY = curr[1] + dirY[j];
+                    
+                    if(newX < m && newX >=0 && newY<n && newY>=0 && !visited[newX][newY]) {
+                        
+                        int dis = Math.max(curr[2], Math.abs(heights[newX][newY] - heights[curr[0]][curr[1]]));
+                        
+                        if(dis < dist[newX][newY]) {
+                            dist[newX][newY] = dis;
+                        }
+                        pq.add(new int[]{newX,newY,dis});
                     }
                 }
             }
         }
-        return 0; // Unreachable code, Java require to return interger value.
+        
+        return dist[m-1][n-1] == Integer.MAX_VALUE ? 0 : dist[m-1][n-1];
     }
 }
