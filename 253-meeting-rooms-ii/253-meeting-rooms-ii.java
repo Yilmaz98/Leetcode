4c33d471bc47a36d64cdd0a6b4b96a1bf8798aa2
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-09-26 02:31:26 -0400

    Time: 15 ms (34.71%), Space: 43.1 MB (86.26%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -1,22 +1,14 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-        
-        int minConferenceRooms = 1;
-    
-        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        queue.offer(new int[]{intervals[0][0], intervals[0][1]});
-        for(int i=1;i<intervals.length;i++) {
-            int endTime = queue.peek()[1];
-            int startTime = intervals[i][0];
-            if(endTime > startTime) {
-                minConferenceRooms++;
-            } else{
-                queue.poll();
-            }
-            queue.offer(new int[]{intervals[i][0],intervals[i][1]});
+        Map<Integer, Integer> m = new TreeMap<>();
+        for (int[] t : intervals) {
+            m.put(t[0], m.getOrDefault(t[0], 0) + 1);
+            m.put(t[1], m.getOrDefault(t[1], 0) - 1);
         }
-        
-        return minConferenceRooms;
+        int res = 0, cur = 0;
+        for (int v : m.values()) {
+            res = Math.max(res, cur += v);
+        }
+        return res;
     }
 }
