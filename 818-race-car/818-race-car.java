882f7124bac368ece27439641ff692618f1be43b
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-09-27 04:02:25 -0400

    Time: 569 ms (20.80%), Space: 208 MB (6.14%) - LeetHub

--- 818-race-car/818-race-car.java
+++ 818-race-car/818-race-car.java
@@ -1,40 +1,61 @@
 class Solution {
-    public int racecar(int target) {
-        int K = 33 - Integer.numberOfLeadingZeros(target - 1);
-        int barrier = 1 << K;
-        int[] dist = new int[2 * barrier + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[target] = 0;
-
-        PriorityQueue<Node> pq = new PriorityQueue<Node>(
-            (a, b) -> a.steps - b.steps);
-        pq.offer(new Node(0, target));
-
-        while (!pq.isEmpty()) {
-            Node node = pq.poll();
-            int steps = node.steps, targ1 = node.target;
-            if (dist[Math.floorMod(targ1, dist.length)] > steps) continue;
-
-            for (int k = 0; k <= K; ++k) {
-                int walk = (1 << k) - 1;
-                int targ2 = walk - targ1;
-                int steps2 = steps + k + (targ2 != 0 ? 1 : 0);
-
-                if (Math.abs(targ2) <= barrier && steps2 < dist[Math.floorMod(targ2, dist.length)]) {
-                    pq.offer(new Node(steps2, targ2));
-                    dist[Math.floorMod(targ2, dist.length)] = steps2;
-                }
-            }
+    class CarAttributes {
+        int position;
+        int speed;
+        CarAttributes(int position, int speed) {
+            this.position = position;
+            this.speed = speed;
         }
-
-        return dist[0];
     }
-}
-
-class Node {
-    int steps, target;
-    Node(int s, int t) {
-        steps = s;
-        target = t;
+    
+    public int getMinimumAttributes(int target) {
+        Queue<CarAttributes> q = new LinkedList<>();
+        q.add(new CarAttributes(0,1));
+        Set<String> visited = new HashSet<>();
+        visited.add(String.valueOf(0) + "," + String.valueOf(1));
+        int minAttributes = 0;
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i=0;i<size;i++) {
+            
+            CarAttributes curr = q.poll();
+                
+                int z = curr.position;
+           // System.out.println(visited);
+                if(curr.position == target)
+                    return minAttributes;
+                
+            //A
+       
+            int nextPosition = curr.position + curr.speed;
+            int nextSpeed = curr.speed * 2;
+            String next = String.valueOf(nextPosition) + "," + String.valueOf(nextSpeed);
+            if(!visited.contains(next) && Math.abs(target - nextPosition) < target) {
+                visited.add(next) ;
+               q.add(new CarAttributes(nextPosition,nextSpeed));  
+            }
+           
+            
+            
+            // R
+            nextSpeed = curr.speed  >= 0 ? -1 : 1;
+            next = String.valueOf(z) + "," + String.valueOf(nextSpeed);
+             if(!visited.contains(next) && Math.abs(target - z) < target) {
+                 visited.add(next);
+                 q.add(new CarAttributes(z, nextSpeed));
+             }
+            
+            }
+            minAttributes++;
+            
+        }
+        
+        return minAttributes;
+    }
+    
+    public int racecar(int target) {
+        return getMinimumAttributes(target);
     }
 }
