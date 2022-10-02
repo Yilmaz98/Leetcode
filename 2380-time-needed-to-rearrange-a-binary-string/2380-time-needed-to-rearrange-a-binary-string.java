4a65349b0dac1a22890cee8f6dbdfa8a1dc42b8b
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-10-02 11:37:22 -0400

    Time: 4 ms (93.49%), Space: 42.6 MB (56.54%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
@@ -1,13 +1,11 @@
 class Solution {
     public int secondsToRemoveOccurrences(String s) {
-        int zc=0,po=0;
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == '0')
-                zc++;
-            else
-                if(zc!=0)
-                    po=Math.max(po+1,zc);
-        }
-        return po;
+          int zeros = 0, seconds = 0;
+    for (int i = 0; i < s.length(); ++i) {
+        zeros += s.charAt(i) == '0' ? 1 : 0;
+        if (s.charAt(i) == '1' && zeros > 0)
+            seconds = Math.max(seconds + 1, zeros);
+    }
+    return seconds;      
     }
 }
