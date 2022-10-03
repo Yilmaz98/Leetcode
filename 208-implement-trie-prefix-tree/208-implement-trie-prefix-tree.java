4df47952972843cf922d94bcc51cc10c359b7550
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-10-02 20:33:20 -0400

    Time: 83 ms (43.72%), Space: 69.2 MB (31.20%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
@@ -1,44 +1,63 @@
-class TrieNode {
-    public boolean isWord; 
-    public TrieNode[] children = new TrieNode[26];
-    public TrieNode() {}
-}
-
-public class Trie {
-    private TrieNode root;
-    public Trie() {
-        root = new TrieNode();
+class Trie {
+    public class TrieNode {
+        boolean isWord;
+        TrieNode[] children;
+        
+        
+        TrieNode() {
+            this.isWord = false;
+            this.children = new TrieNode[26];
+        }
+        
+        TrieNode(boolean isWord, TrieNode[] children) {
+            this.isWord = isWord;
+            this.children = children;
+        }
     }
-
+    
+    TrieNode root = new TrieNode();
+    public Trie() {
+     
+    }
+    
     public void insert(String word) {
         TrieNode ws = root;
-        for(int i = 0; i < word.length(); i++){
-            char c = word.charAt(i);
-            if(ws.children[c - 'a'] == null){
-                ws.children[c - 'a'] = new TrieNode();
-            }
-            ws = ws.children[c - 'a'];
+        for(char c : word.toCharArray()) {
+            if(ws.children[c-'a'] == null) {
+                ws.children[c-'a'] = new TrieNode();
+            } 
+            ws = ws.children[c-'a'];
         }
         ws.isWord = true;
     }
-
+    
     public boolean search(String word) {
-        TrieNode ws = root; 
-        for(int i = 0; i < word.length(); i++){
-            char c = word.charAt(i);
-            if(ws.children[c - 'a'] == null) return false;
-            ws = ws.children[c - 'a'];
+        TrieNode ws = root;
+        for(char c : word.toCharArray()) {  
+            if(ws.children[c-'a'] == null) {
+                return false;
+            } 
+            ws = ws.children[c-'a'];
         }
-        return ws.isWord;
+       return ws.isWord;
     }
-
+    
     public boolean startsWith(String prefix) {
-        TrieNode ws = root; 
-        for(int i = 0; i < prefix.length(); i++){
-            char c = prefix.charAt(i);
-            if(ws.children[c - 'a'] == null) return false;
-            ws = ws.children[c - 'a'];
+        TrieNode ws = root;
+        for(char c : prefix.toCharArray()) {  
+            if(ws.children[c-'a'] == null) {
+                return false;
+            } 
+            ws = ws.children[c-'a'];
         }
-        return true;
+       return true;
     }
 }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
