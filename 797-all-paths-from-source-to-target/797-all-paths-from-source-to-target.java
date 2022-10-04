53452f08fb58669a8373d4b391bdca5097cf59fb
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-10-04 00:06:10 -0400

    Time: 41 ms (5.42%), Space: 55.9 MB (8.51%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -0,0 +1,58 @@
+class Node {
+    Integer val;
+    List<Integer> path = new ArrayList<>();
+    
+    Node(int ni, List<Integer> arr) {
+        val = ni;
+        path = arr;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        int n = graph.length;
+        
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        
+        for(int i=0;i<graph.length;i++) {
+            adj.put(i , new ArrayList<>());
+            
+            for(int node : graph[i]) {
+                adj.get(i).add(node);
+            }
+        }
+        //System.out.println(adj);
+        
+        PriorityQueue<Node> q = new PriorityQueue<>((a,b)->a.val - b.val);
+        List<Integer> start = new ArrayList<>();
+        start.add(0);
+        q.add(new Node(0, start));
+        Set<Integer> visited = new HashSet<>();
+        visited.add(0);
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            //System.out.println(q);
+            while(size-->0) {
+            Node current = q.poll();
+                
+            if(current.val == n-1) {
+                result.add(current.path);
+            }
+            
+            List<Integer> neighbours = adj.get(current.val);
+            for(int i=0;i<neighbours.size();i++) {
+                if(!visited.contains(neighbours.get(i))) {
+                    List<Integer> currPath = new ArrayList<>(current.path);
+                    currPath.add(neighbours.get(i));
+                    q.add(new Node(neighbours.get(i), currPath));
+                }
+            }                  
+            }
+        }
+        
+        return result;
+    }
+}
