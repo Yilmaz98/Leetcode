8f2f103d5897c8202242d1cae1b41ff893c5258c
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-10-04 00:09:46 -0400

    Time: 3 ms (88.77%), Space: 55.3 MB (39.83%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,57 +1,24 @@
-class Node {
-    Integer val;
-    List<Integer> path = new ArrayList<>();
-    
-    Node(int ni, List<Integer> arr) {
-        val = ni;
-        path = arr;
-    }
-}
-
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        int n = graph.length;
-        
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-        
-        for(int i=0;i<graph.length;i++) {
-            adj.put(i , new ArrayList<>());
-            
-            for(int node : graph[i]) {
-                adj.get(i).add(node);
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        backtracking(res, graph, 0, path);
+        return res;
+    }
+    
+    private void backtracking(List<List<Integer>> res, int[][] graph, int start, List<Integer> cur) {
+        if(graph[start].length==0 || start==graph.length-1) {
+            if(start==graph.length-1) {
+                res.add(new ArrayList<>(cur));
             }
+            return;
         }
         
-        PriorityQueue<Node> q = new PriorityQueue<>((a,b)->a.val - b.val);
-        List<Integer> start = new ArrayList<>();
-        start.add(0);
-        q.add(new Node(0, start));
-        Set<Integer> visited = new HashSet<>();
-        visited.add(0);
-        
-        List<List<Integer>> result = new ArrayList<>();
-        
-        while(!q.isEmpty()) {
-            int size = q.size();
-            while(size-->0) {
-            Node current = q.poll();
-            List<Integer> currPath = current.path;
-                
-            if(current.val == n-1) {
-                result.add(currPath);
-            }
-            
-            List<Integer> neighbours = adj.get(current.val);
-            for(int i=0;i<neighbours.size();i++) {
-                if(!visited.contains(neighbours.get(i))) {
-                    currPath = new ArrayList<>(current.path);
-                    currPath.add(neighbours.get(i));
-                    q.add(new Node(neighbours.get(i), currPath));
-                }
-            }                  
-            }
+        for(int next:graph[start]) {
+            cur.add(next);
+            backtracking(res, graph, next, cur);
+            cur.remove(cur.size()-1);
         }
-        
-        return result;
     }
 }
