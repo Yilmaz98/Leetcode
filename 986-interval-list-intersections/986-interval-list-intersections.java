d7cd71ee76e6c10eb4c0275679303b69fd384572
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-10-06 02:58:48 -0400

    Time: 16 ms (15.87%), Space: 43.2 MB (90.62%) - LeetHub

---
+++ 986-interval-list-intersections/986-interval-list-intersections.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+    	PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0]==b[0] ? b[1]-a[1]: a[0]-b[0] );
+        
+    	List<int[]> res = new ArrayList<>();
+        
+    	for (int[] first : firstList) {
+    		pq.add(new int[] {first[0], 1});
+    		pq.add(new int[] {first[1], -1});
+    	}
+        
+    	for (int[] second : secondList) {
+    		pq.add(new int[] {second[0], 1});
+    		pq.add(new int[] {second[1], -1});
+    	}
+    	
+    	int sum = 0;
+        
+    	while(!pq.isEmpty()) {
+    		int[] pqVal = pq.poll();
+    		sum+= pqVal[1];
+    		if (sum >= 2) {
+    			res.add(new int[] {pqVal[0], pq.peek()[0]});
+    		}
+    	}
+    	
+    	int[][] resVal = new int[res.size()][2];
+        
+    	for (int i=0; i<res.size(); i++) {
+    		resVal[i][0] = res.get(i)[0];
+    		resVal[i][1] = res.get(i)[1];
+    	}
+    	
+    	return resVal;
+    }
+}
