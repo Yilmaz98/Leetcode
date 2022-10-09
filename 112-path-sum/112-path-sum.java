db336f61a3454b5535d2eab491ad099784de05b6
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-10-09 01:18:44 -0400

    Time: 3 ms (6.88%), Space: 44.7 MB (6.85%) - LeetHub

---
+++ 112-path-sum/112-path-sum.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class QNode{
+    TreeNode node;
+    int sum;
+    QNode(TreeNode node, int sum) {
+        this.node = node;
+        this.sum = sum;
+    }
+    
+}
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) {
+            return false;
+        }
+        int sum = 0;
+        Queue<QNode> q = new LinkedList<>();
+        q.offer(new QNode(root,root.val));
+        
+        while(!q.isEmpty()) {
+            QNode curr = q.poll();
+            TreeNode n = curr.node;
+            sum = curr.sum;
+            
+            if(n.left !=null) {
+                q.offer(new QNode(n.left,n.left.val + sum));
+            }
+            
+            if(n.right !=null) {
+                q.offer(new QNode(n.right,n.right.val + sum));
+            }
+            
+            if(n.left == null && n.right == null) {
+                if(sum == targetSum) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
