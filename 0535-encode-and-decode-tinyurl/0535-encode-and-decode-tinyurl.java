3431289694d9417393a348abf8b55b53cbdea5d6
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2022-11-10 00:32:38 -0500

    Time: 13 ms (20.97%), Space: 44.3 MB (7.10%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -1,13 +1,15 @@
 public class Codec {
-
+    Map<Integer, String> map = new HashMap<>();
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        return longUrl;
+        map.put(longUrl.hashCode(), longUrl);
+        return "http://tinyurl.com/" + longUrl.hashCode();
+        
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortUrl;
+       return map.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
     }
 }
 
