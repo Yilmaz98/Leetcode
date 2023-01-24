c740307c99eda4d2f111da9d20406cbb7b425336
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-01-24 09:26:10 -0500

    Time: 0 ms (100.00%), Space: 43.4 MB (7.27%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,31 +1,20 @@
 class Solution {
-  /*
-  Dutch National Flag problem solution.
-  */
-  public void sortColors(int[] nums) {
-    // for all idx < i : nums[idx < i] = 0
-    // j is an index of element under consideration
-    int p0 = 0, curr = 0;
-    // for all idx > k : nums[idx > k] = 2
-    int p2 = nums.length - 1;
-
-    int tmp;
-    while (curr <= p2) {
-      if (nums[curr] == 0) {
-        // swap p0-th and curr-th elements
-        // i++ and j++
-        tmp = nums[p0];
-        nums[p0++] = nums[curr];
-        nums[curr++] = tmp;
-      }
-      else if (nums[curr] == 2) {
-        // swap k-th and curr-th elements
-        // p2--
-        tmp = nums[curr];
-        nums[curr] = nums[p2];
-        nums[p2--] = tmp;
-      }
-      else curr++;
+  public void sortColors(int[] A) {
+      int n = A.length;
+     int n0 = -1, n1 = -1, n2 = -1;
+    for (int i = 0; i < n; ++i) {
+        if (A[i] == 0) 
+        {
+            A[++n2] = 2; A[++n1] = 1; A[++n0] = 0;
+        }
+        else if (A[i] == 1) 
+        {
+            A[++n2] = 2; A[++n1] = 1;
+        }
+        else if (A[i] == 2) 
+        {
+            A[++n2] = 2;
+        }
     }
   }
 }
