fd6d4323d42c0987818a158b56af88aabf54b1e7
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-01-24 09:22:36 -0500

    Time: 1 ms (38.42%), Space: 42.4 MB (32.44%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,25 +1,31 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int count[] = new int[3];
-        
-        for(int i=0;i<nums.length;i++) {
-            count[nums[i]]++;
-        }
-        
-        int index = 0;
-        
-        for(int i=0;i<count[0];i++) {
-            nums[index++] = 0;
-        }
-        
-        for(int i=0;i<count[1];i++) {
-            nums[index++] = 1;
-        }
-        
-        
-        for(int i=0;i<count[2];i++) {
-            nums[index++] = 2;
-        }
-        
+  /*
+  Dutch National Flag problem solution.
+  */
+  public void sortColors(int[] nums) {
+    // for all idx < i : nums[idx < i] = 0
+    // j is an index of element under consideration
+    int p0 = 0, curr = 0;
+    // for all idx > k : nums[idx > k] = 2
+    int p2 = nums.length - 1;
+
+    int tmp;
+    while (curr <= p2) {
+      if (nums[curr] == 0) {
+        // swap p0-th and curr-th elements
+        // i++ and j++
+        tmp = nums[p0];
+        nums[p0++] = nums[curr];
+        nums[curr++] = tmp;
+      }
+      else if (nums[curr] == 2) {
+        // swap k-th and curr-th elements
+        // p2--
+        tmp = nums[curr];
+        nums[curr] = nums[p2];
+        nums[p2--] = tmp;
+      }
+      else curr++;
     }
+  }
 }
