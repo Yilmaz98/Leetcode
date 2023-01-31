c36302903574b0ce38ba93ae1134d7cecbda273e
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-01-31 18:31:30 -0500

    Time: 1 ms (88.55%), Space: 40.5 MB (98.11%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,37 +1,28 @@
 class Solution {
-    private List<String> combinations = new ArrayList<>();
-    private Map<Character, String> letters = Map.of(
-        '2', "abc", '3', "def", '4', "ghi", '5', "jkl", 
-        '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
-    private String phoneDigits;
+    Map<Character, String> m = Map.of('2',"abc",'3',"def",'4',"ghi",'5',"jkl",'6', "mno", '7', "pqrs", '8',"tuv", '9', "wxyz");
+    String phoneDigits;
+    List<String> combinations = new ArrayList<>();
     
     public List<String> letterCombinations(String digits) {
-        // If the input is empty, immediately return an empty answer array
-        if (digits.length() == 0) {
-            return combinations;
-        }
-        
-        // Initiate backtracking with an empty path and starting index of 0
+        if(digits.length() == 0)
+            return new ArrayList<>();
+            
         phoneDigits = digits;
         backtrack(0, new StringBuilder());
+        
         return combinations;
     }
     
-    private void backtrack(int index, StringBuilder path) {
-        // If the path is the same length as digits, we have a complete combination
-        if (path.length() == phoneDigits.length()) {
+    public void backtrack(int index, StringBuilder path) {
+        if(path.length() == phoneDigits.length()) {
             combinations.add(path.toString());
-            return; // Backtrack
+            return;
         }
         
-        // Get the letters that the current digit maps to, and loop through them
-        String possibleLetters = letters.get(phoneDigits.charAt(index));
-        for (char letter: possibleLetters.toCharArray()) {
-            // Add the letter to our current path
+        String possibleCharacters = m.get(phoneDigits.charAt(index));
+        for(char letter : possibleCharacters.toCharArray()) {
             path.append(letter);
-            // Move on to the next digit
-            backtrack(index + 1, path);
-            // Backtrack by removing the letter before moving onto the next
+            backtrack(index+1,path);
             path.deleteCharAt(path.length() - 1);
         }
     }
