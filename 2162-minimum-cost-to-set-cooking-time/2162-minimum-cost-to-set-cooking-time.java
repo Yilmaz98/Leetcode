ba5cde25b7a98ca260501c378da8d4da8785539d
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-04-04 00:04:19 -0400

    Time: 3 ms (18.00%), Space: 39.8 MB (40.00%) - LeetHub

--- 2162-minimum-cost-to-set-cooking-time/2162-minimum-cost-to-set-cooking-time.java
+++ 2162-minimum-cost-to-set-cooking-time/2162-minimum-cost-to-set-cooking-time.java
@@ -1,38 +1,52 @@
 class Solution {
     public int minCostSetTime(int startAt, int moveCost, int pushCost, int targetSeconds) {
-        int minutes = targetSeconds / 60;
-        int seconds = targetSeconds % 60;
-
-        List<String> all = new ArrayList<>();
-        if(minutes < 100) {
-            int first = (minutes * 100) + seconds;
-            all.add(String.valueOf(first));
-        }
-
-        if(seconds < 40) {
-            minutes = minutes - 1;
-            seconds = seconds + 60;
-            int second = minutes * 100 + seconds;
-            all.add(String.valueOf(second));
-        }
+            int finalCost = 0;
+         char start = String.valueOf(startAt).charAt(0);
         
-        int bestCost  = Integer.MAX_VALUE;
-
-        for(String curr : all) {
-            int cost = 0;
-            char tempStartAt = (char) (startAt + '0');
-            for(int j = 0;j<curr.length();j++) {
-                if(curr.charAt(j) != tempStartAt)    {
-                tempStartAt = curr.charAt(j);
-                cost += moveCost;
-                cost += pushCost;
-            } else {
-                cost += pushCost;
+            int roundMins = targetSeconds/60;
+            int roundSecs = targetSeconds%60;
+            
+            int minusMins = roundMins;
+            int minusSecs = roundSecs;  
+            
+            if(roundSecs < 40) {
+              minusMins = roundMins - 1;
+              minusSecs = roundSecs + 60;   
             }
+           
+            
+            String roundStr = String.valueOf(roundMins*100+roundSecs);
+            String minusStr = String.valueOf(minusMins*100+minusSecs);
+            
+            if(start != roundStr.charAt(0))
+               finalCost += moveCost;
+            
+            for(int i=0;i<roundStr.length()-1;i++) {
+                if(roundStr.charAt(i) != roundStr.charAt(i+1))
+                    finalCost += moveCost;
+                finalCost += pushCost;
             }
-            bestCost = Math.min(cost, bestCost);
-        }
-
-        return bestCost;
+            finalCost += pushCost;
+            
+            System.out.println(finalCost);
+            
+            int finalCost2 = 0;
+            
+            if(start != minusStr.charAt(0))
+               finalCost2 += moveCost;
+            
+            for(int i=0;i<minusStr.length()-1;i++) {
+                if(minusStr.charAt(i) != minusStr.charAt(i+1))
+                    finalCost2 += moveCost;
+                finalCost2 += pushCost;
+            }
+            finalCost2 += pushCost;
+            
+           if(targetSeconds > 5999)
+               return finalCost2;
+            
+            finalCost = Math.min(finalCost, finalCost2);
+        
+        return finalCost;
     }
 }
