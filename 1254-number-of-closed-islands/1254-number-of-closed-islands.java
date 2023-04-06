b4f3f35c9717ae33ba6e7800e7d0fff21d69e3e7
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-04-06 00:49:34 -0400

    Time: 2 ms (82.36%), Space: 43.1 MB (43.02%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -34,13 +34,6 @@ class Solution {
             }
         }
         
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                System.out.print(grid[i][j] + " ");
-            }
-            System.out.println("");
-        }
-        
         boolean[][] visited = new boolean[m][n];
         int islands = 0;
         
