69e00ffead4f241a7ee5a1c815eed7fdf9aed6f5
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-04-07 02:39:40 -0400

    Time: 123 ms (37.65%), Space: 66.1 MB (15.51%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,29 +1,38 @@
 class Solution {
-    public int dfs(Map<Integer,List<Integer>> m, int headID, int[] informTime) {       
-        int max = 0;
-        if(!m.containsKey(headID)) {
-            return informTime[headID];
-        }
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        Queue<int[]> q = new LinkedList<>();
         
-        for(int i=0;i<m.get(headID).size();i++) {
-            max = Math.max(max, informTime[headID] +  dfs(m,m.get(headID).get(i),informTime));
-        }    
-        return max;  
-    } 
-    
-    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime)     {
         Map<Integer, List<Integer>> m = new HashMap<>();
         
         for(int i=0;i<manager.length;i++) {
-            if(manager[i]<0)
+            if(manager[i] == -1)
                 continue;
             if(!m.containsKey(manager[i])) {
                 m.put(manager[i], new ArrayList<>());
             }
-            
             m.get(manager[i]).add(i);
         }
         
-        return dfs(m, headID, informTime);
+        int result = 0;
+        
+        q.add(new int[]{headID, informTime[headID]});
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i=0;i<size;i++) {
+            int[] curr = q.poll();
+                if(!m.containsKey(curr[0]))
+                    continue;
+                
+                for(Integer neigh : m.get(curr[0])) {
+                    int ans = informTime[neigh] + curr[1];
+                    result = Math.max(result, ans);
+                    q.add(new int[]{neigh, ans});
+                }
+            }
+        }
+        
+        return result;
     }
 }
