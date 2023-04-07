8869755700f168a215580038cefc7743de7db12f
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-04-07 01:01:37 -0400

    Time: 9 ms (77.98%), Space: 56 MB (36.81%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,49 @@
+class Solution {
+    public void dfs(int i, int j, int[][] grid, int m, int n) {
+        if(i<0 || j<0 || i>m-1 || j>n-1 || grid[i][j] == 0)
+            return;
+        
+        grid[i][j] = 0;
+        dfs(i+1,j,grid,m,n);
+        dfs(i,j+1,grid,m,n);
+        dfs(i,j-1,grid,m,n);
+        dfs(i-1,j,grid,m,n);
+    }
+    
+      public void dfs2(int i, int j, int m, int n, int[][] grid, boolean[][] visited) {
+        if(i<0 || j<0 || i>m-1 || j>n-1 || visited[i][j] || grid[i][j] == 1)
+            return;
+        
+        visited[i][j] = true;
+          
+        dfs(i+1,j,grid,m,n);
+        dfs(i,j+1,grid,m,n);
+        dfs(i,j-1,grid,m,n);
+        dfs(i-1,j,grid,m,n);
+    }
+    
+    public int numEnclaves(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        for(int i=0;i<grid.length;i++) {
+            for(int j=0;j<grid[0].length;j++) {
+                if((i==0 || j==0 || i == m-1 || j == n-1) && grid[i][j] == 1)           {
+                    dfs(i,j,grid, m,n);
+                }
+            }
+        }
+        
+        int islands = 0;
+        
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+               if(grid[i][j] == 1) {
+                  islands++;
+               }
+            }
+        }
+        
+        return islands;
+    }
+}
