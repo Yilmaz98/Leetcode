39e0fbea9cfd242da06b1474965fc852b3baeaae
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-04-13 02:24:32 -0400

    Time: 7 ms (6.20%), Space: 42.8 MB (13.68%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> st = new Stack<>();
+        
+        int idx = 0;
+        for(int i=0;i<pushed.length;i++) {
+            st.push(pushed[i]);
+            while(!st.isEmpty() && st.peek() == popped[idx]) {
+                st.pop();
+                idx++;
+            } 
+        }
+        
+        System.out.println(st);
+        
+        while(idx < popped.length) {
+            if(st.pop() != popped[idx]) {
+                return false;
+            } 
+            idx++;
+        }
+            
+        
+        return true;
+    }
+}
