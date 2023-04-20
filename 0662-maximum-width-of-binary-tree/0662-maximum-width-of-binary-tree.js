f67a882bcc796dc6fada3e398c0734b029a469d6
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-04-20 01:55:02 -0400

    Time: 64 ms (96.60%), Space: 46.7 MB (71.43%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.js
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.js
@@ -1,51 +1,20 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
 var widthOfBinaryTree = function(root) {
-    let maxA = 0n, hash=new Map();
-    const recurse = (cur, width, level) =>
-    {
-        if(cur===null)
-        {
-            return;
-        }
-        if(hash.has(level))
-        { 
-            // console.log(hash);
-            let [min, max] = hash.get(level);
-            if ((BigInt)(min) > (BigInt) (width))
-                min = (BigInt) (width);
-            else 
-                min = (BigInt) (min); 
-            
-             if ((BigInt) (max) <  (BigInt) (width))
-                max = (BigInt) (width);
-            else 
-                max = (BigInt) (max); 
-    
-            hash.set(level, [min, max]);
-        }
-        else{
+    const nodes = {};
+    let maxWidth = 0;
 
-            hash.set(level, [(width), (width)]);
-        }
+    var findMaxWidth = (root, count, index) => {
+        if (!root) return;
+        if (!nodes.hasOwnProperty(count))
+            nodes[count] = index;
 
-        if((BigInt) (hash.get(level)[1]) - (BigInt) (hash.get(level)[0]) +1n > maxA)
-            maxA = (BigInt) (hash.get(level)[1]) - (BigInt) (hash.get(level)[0]) + 1n;
-        else 
-            maxA= (BigInt) (maxA);
-        recurse(cur.left, (BigInt)  (width)*2n-1n, level+1);
-        recurse(cur.right, (BigInt) (width) *2n, level+1);
-    }
-    recurse(root,1, 0)
-    return maxA;
+        const idx = index - nodes[count];
+
+        maxWidth = Math.max(maxWidth, idx + 1);
+
+        findMaxWidth(root.left, count + 1, 2 * idx);
+        findMaxWidth(root.right, count + 1, 2 * idx + 1);
+    };
+
+    findMaxWidth(root, 0, 0);
+    return maxWidth;
 };
