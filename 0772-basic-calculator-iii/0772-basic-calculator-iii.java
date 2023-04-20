5748af265b163b9f319900ebbbd2cbf5cd8786d3
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-04-20 00:01:41 -0400

    Time: 7 ms (40.66%), Space: 40.7 MB (84.62%) - LeetHub

--- 0772-basic-calculator-iii/0772-basic-calculator-iii.java
+++ 0772-basic-calculator-iii/0772-basic-calculator-iii.java
@@ -1,64 +1,63 @@
 class Solution {
-   public int calculate(String s) {
-        //time:O(N), space:O(N)
-        if (s == null || s.length() == 0) return 0; 
-        Stack<Integer> nums = new Stack<>();    // the stack that stores numbers
-        Stack<Character> ops = new Stack<>();   // the stack that stores operators (including parentheses)
-        int num = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == ' ') {
-                continue;
-            }
-            if (Character.isDigit(c)) {
-                num = c - '0';
-                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
-                    num = 10 * num + (s.charAt(i + 1) - '0');
+    public int calculate(String s) {
+        if(s.length() == 0 || s == null) return 0;
+        
+        Stack<Integer> nums = new Stack<>();
+        Stack<Character> ops = new Stack<>();
+        int operand = 0;
+        
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch)) {
+                operand = (ch - '0');
+                while(i + 1< s.length() && Character.isDigit(s.charAt(i+1))) {
+                    operand = (operand*10) + (s.charAt(i+1) - '0');
                     i++;
                 }
-                nums.push(num);
-                num = 0; // reset the number to 0 before next calculation
-            } else if (c == '(') {
-                ops.push(c);
-            } else if (c == ')') {
-                while (ops.peek() != '(') {
+                nums.push(operand);
+                operand = 0;
+            } 
+            else if(ch == '(') {
+                ops.push(ch);
+            } else if(ch == ')') {
+                while(ops.peek() != '(') {
                     nums.push(operation(nums.pop(), ops.pop(), nums.pop()));
                 }
                 ops.pop();
-            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-                while (!ops.isEmpty() && hasPrecedence(ops.peek(), c)) {
+            } else if(ch == '+' || ch == '-' || ch == '*' || ch =='/') {
+                while(!ops.isEmpty() && hasPrecedence(ops.peek(), ch)) {
                     nums.push(operation(nums.pop(), ops.pop(), nums.pop()));
                 }
-                // Dealing with the negative number
-
-                ops.push(c);
+                ops.push(ch);
             }
         }
-        while (!ops.isEmpty()) {
-           nums.push(operation(nums.pop(), ops.pop(), nums.pop()));
+        
+        while(!ops.isEmpty())
+        {
+            nums.push(operation(nums.pop(), ops.pop(), nums.pop()));
         }
+        
         return nums.pop();
     }
     
-    // Notice b is before a, since we pop b first
-    private int operation(int b, char op, int a) {
-        switch (op) {
-            case '+': return a + b;
-            case '-': return a - b;
-            case '*': return a * b;
-            case '/': return a / b; //assume b is not 0
+    public int operation(int a, char op, int b) {
+        switch(op) {
+            case '+': return b + a;
+            case '-': return b - a;
+            case '*': return b * a;
+            case '/': return b / a;
         }
         return 0;
     }
     
-    // helper function to check precedence of the uppermost operator in the ops stack and current operator
-    private boolean hasPrecedence(char op1, char op2) {
-        if (op1 == '(' || op1 == ')') {
+    public boolean hasPrecedence(char op1, char op2) {
+        if(op1 == '(' || op1 == ')')
+            return false;
+        
+        if((op1 == '+' || op1 == '-') && (op2 == '*' || op2 == '/')) {
             return false;
         }
-        if ((op1 == '+' || op1 == '-') && (op2 == '*' || op2 == '/')) {
-            return false;
-        }
+        
         return true;
     }
 }
