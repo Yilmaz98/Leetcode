0793323894b23515240bf3f6033e91603d9f9de1
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-04-26 01:04:44 -0400

    Time: 34 ms (23.45%), Space: 51.1 MB (51.55%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -3,9 +3,13 @@ class Solution {
         Map<Integer, List<Integer>> adj = new HashMap<>();
         
         int[] outdegree = new int[graph.length];
+                Queue<Integer> q = new LinkedList<>();  
         
         for(int i=0;i<graph.length;i++) { 
             outdegree[i] = graph[i].length;
+             if(outdegree[i] == 0)  {
+                q.add(i);
+            }
            for(int j=0;j<graph[i].length;j++) {
                 if(!adj.containsKey(graph[i][j])) {
                     adj.put(graph[i][j], new ArrayList<>());
@@ -14,23 +18,15 @@ class Solution {
            }
         } 
         
-        Queue<Integer> q = new LinkedList<>();  
+
         List<Integer> result = new ArrayList<>();
-        
-        for(int i=0;i<outdegree.length;i++) {
-            if(outdegree[i] == 0)  {
-                q.add(i);
-            }
-        }
+
         
         while(!q.isEmpty()) {
             int size = q.size();
-            
             for(int i=0;i<size;i++) {
-                  
             Integer curr = q.poll();
             
-
             if(!adj.containsKey(curr))
                 continue;
             
