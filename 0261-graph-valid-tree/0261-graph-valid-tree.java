9146eb9fa41ff3afbc13560eedb0dbf8dc8b77fa
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-04-27 15:09:47 -0400

    Time: 10 ms (8.51%), Space: 42.7 MB (75.07%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {     
+        if(n == 1 && edges.length == 0)
+            return true;
+        
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        
+        for(int[] edge: edges) {
+            if(!adj.containsKey(edge[0]))
+                adj.put(edge[0], new ArrayList<>());
+            
+            if(!adj.containsKey(edge[1]))
+                adj.put(edge[1], new ArrayList<>());
+            
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        q.add(0);
+        
+        while(!q.isEmpty()) {
+            Integer curr = q.poll();
+            if(visited.contains(curr))
+                return false;
+            visited.add(curr);
+            
+            if(!adj.containsKey(curr))
+                return false;
+            
+            for(Integer nei : adj.get(curr)) {
+                if(!visited.contains(nei))
+                    q.add(nei);
+            }
+        }
+        
+        if(visited.size() < n)
+            return false;
+        
+        return true;
+        
+    }
+}
