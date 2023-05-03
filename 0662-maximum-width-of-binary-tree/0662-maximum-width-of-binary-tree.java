7dcf5f1a401e2e134495d669decb726c71ae4816
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-05-02 23:25:28 -0400

    Time: 1 ms (100.00%), Space: 42.6 MB (23.24%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -14,36 +14,36 @@
  * }
  */
 class Solution {
-    Map<Integer, List<Long>> m = new HashMap<>();
     public int widthOfBinaryTree(TreeNode root) {
-        if(root == null)
-            return 0;
-        long maxWidth = 1;
-        inorderUtil(root, 1, 1);
-        System.out.println(m);
-        for(Integer i: m.keySet()) {
-            long currWidth = 1;
-            currWidth = m.get(i).get(1) - m.get(i).get(0) + 1;
-            maxWidth = Math.max(maxWidth, currWidth);
-        }
-        return (int) maxWidth;
-    }
-    
-    public void inorderUtil(TreeNode root, long width, int level) {
-        if(root != null) {
-            if(!m.containsKey(level)) {
-                m.put(level, new ArrayList<>());
-                m.get(level).add(width);
-                m.get(level).add(width);
-            } else {
-                 long min = Math.min(m.get(level).get(0),width);
-                 long max = Math.max(m.get(level).get(1),width);
-                 m.get(level).set(0, min);
-                 m.get(level).set(1, max);
+        Queue<TreeNode> q = new LinkedList<>();
+        Queue<Integer> qIndex = new LinkedList<>();
+        q.add(root);
+        qIndex.add(1); //store index, assuming root's index is 1
+        int max = 0;
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            int start = 0, end = 0;
+            for(int i=0; i<size; i++)
+            {
+                TreeNode node = q.remove();
+                int index = qIndex.remove();
+                if(i==0) start = index; //start and end index for each level
+                if(i==size-1) end = index;
+                if(node.left!=null)
+                {
+                    q.add(node.left);
+                    qIndex.add(2*index);
+                }
+                
+                if(node.right!=null)
+                {
+                    q.add(node.right);
+                    qIndex.add(2*index+1);
+                }
             }
-           
-            inorderUtil(root.left, 2* width -1, level+1);
-            inorderUtil(root.right, 2* width,level+1);
-        }  
+            max = Math.max(max, end - start + 1);
+        }
+        return max;    
     }
 }
