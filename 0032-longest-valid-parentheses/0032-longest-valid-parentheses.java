395617e09def32a143763eb760a62a403378dae9
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-05-08 00:03:17 -0400

    Time: 5 ms (66.52%), Space: 42.7 MB (33.03%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -22,7 +22,6 @@ class Solution {
         int maxAns = 0;
         
         for(int i=0;i<arr.length;i++) {
-            System.out.print(arr[i]);
             if(arr[i] == -1) {
                     ans = 0;
             } else {
