27d0bc48c891fd75fab933ddf0663ad91d946265
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-05-08 00:03:09 -0400

    Time: 148 ms (5.04%), Space: 43.3 MB (6.31%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> st = new Stack<>();
+        
+        int[] arr = new int[s.length()];
+        Arrays.fill(arr,-1);
+        
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i) == '(') {
+                st.push(i);
+            } else {
+                    if(!st.isEmpty()) {
+                         arr[st.pop()] = 1;
+                         arr[i] = 1;
+                    } else{
+                        continue;
+                    }
+                }
+            }
+        
+        int ans = 0;
+        int maxAns = 0;
+        
+        for(int i=0;i<arr.length;i++) {
+            System.out.print(arr[i]);
+            if(arr[i] == -1) {
+                    ans = 0;
+            } else {
+            ans += arr[i];
+            maxAns = Math.max(maxAns, ans); 
+            }
+        }
+        
+        return maxAns;
+    }
+}
