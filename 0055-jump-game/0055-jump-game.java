c73f9d00bac6fff9cc87d8d2121e694b570e3b99
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-05-18 00:37:18 -0400

    Time: 2 ms (76.29%), Space: 42.6 MB (98.10%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int maxJump = 0;
-        if(nums.length == 1)
-            return true;
-        for(int i=0;i<nums.length-1;i++) {
-            if(i > maxJump)
-                return false;
-            maxJump = Math.max(i + nums[i], maxJump);
-            if(maxJump >= nums.length-1)
-                return true;
+        
+        for(int i=0;i<nums.length;i++) {
+           if(i > maxJump)
+               return false;
+            
+           maxJump = Math.max(maxJump, i+nums[i]);
         }
         
-        return false;
+        return true;
     }
 }
