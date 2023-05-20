f1af70a5b064b2461dde07b8254112299d41f8a8
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-05-20 03:02:23 -0400

    Time: 1 ms (99.90%), Space: 41.9 MB (87.69%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,17 +1,18 @@
 class Solution {
     private boolean valid(String s, int start, int length) {
-    return length == 1 || (s.charAt(start) != '0' && (length < 3 ||  s.substring(start, start + length).compareTo("255") <= 0));
+        return length == 1 || 
+            (s.charAt(start) != '0' && 
+             (length < 3 || 
+              s.substring(start, start + length).compareTo("255") <= 0));
     }
     
     private void helper(String s, int startIndex, List<Integer> dots, List<String> ans) {
         final int remainingLength = s.length() - startIndex;
         final int remainingNumberOfIntegers = 4 - dots.size();
-        
         if (remainingLength > remainingNumberOfIntegers * 3 || 
             remainingLength < remainingNumberOfIntegers) {
             return;
         }
-        
         if (dots.size() == 3) {
             if (valid(s, startIndex, remainingLength)) {
                 StringBuilder sb = new StringBuilder();
@@ -26,15 +27,14 @@ class Solution {
             }
             return;
         }
-        
         for (int curPos = 1; curPos <= 3 && curPos <= remainingLength; ++curPos) {
-         
+            // Append a dot at the current position.
             dots.add(curPos);
-           
+            // Try making all combinations with the remaining string.
             if (valid(s, startIndex, curPos)) {
                 helper(s, startIndex + curPos, dots, ans);
             }
-            
+            // Backtrack, i.e. remove the dot to try placing it at the next position.
             dots.remove(dots.size() - 1);
         }
     }
