9c03bf292a50d4a4549b65c3c57072ca9ccbc371
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-05-20 22:05:15 -0400

    Time: 246 ms (42.76%), Space: 65.2 MB (5.05%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+        
+        TreeMap<Integer,Integer> m = new TreeMap<>();
+        
+        for(int num : nums) {
+            m.put(num, m.getOrDefault(num,0) + 1);
+        }
+        
+        int prev = Integer.MIN_VALUE;
+        int count = 1;
+        int ans = 1;
+        
+        System.out.println(m);
+        
+        for(Integer v : m.keySet()) {
+            if(prev == Integer.MIN_VALUE) {
+                prev = v;
+            }
+            else {
+                if(v - prev == 1) {
+                    count++;
+                    ans = Math.max(ans, count);
+                } else {
+                    count = 1;
+                }
+            }
+            prev = v;
+        }
+        
+        return ans;
+    }
+}
