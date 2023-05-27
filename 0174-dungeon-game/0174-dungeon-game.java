540330fe0dd171c131c69b07d0881fa408073740
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-05-27 00:18:29 -0400

    Time: 63 ms (5.07%), Space: 43.6 MB (8.95%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -6,7 +6,14 @@ class Solution {
         int[][] memo = new int[m][n];
         
         helper(0,0,dungeon,memo);
-              
+        
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                System.out.print(memo[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
         return memo[0][0];
     }
 
