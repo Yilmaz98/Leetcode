0a174f7379addc27c7852bd898bcb75f7c8ee8f0
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-05-30 20:57:07 -0400

    Time: 38 ms (21.91%), Space: 45.2 MB (5.79%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,17 +1,26 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp, amount+1);
-        dp[0] = 0;
-        
-        for(int i=0;i<=amount;i++) {
-            for(int j=0;j<coins.length;j++) {
-                if(coins[j] <= i) {
-                    dp[i] = Math.min(dp[i],dp[i - coins[j]] + 1);
-                }
-            }
+        int n = coins.length;  
+        int[][] memo = new int[n][amount + 1];
+
+        int ans = helper(0, amount, coins, memo);
+            
+        return ans == Integer.MAX_VALUE -1? -1 : ans;
+    }
+    
+    public int helper(int i, int sum,int[] coins, int[][] memo) {
+        if(sum <0 || i == coins.length) {
+            return Integer.MAX_VALUE -1 ;
         }
         
-        return dp[amount] == amount +1 ? -1: dp[amount];
+      
+        if(sum == 0) {
+            return 0;
+        }
+    
+        if(memo[i][sum] != 0)
+            return memo[i][sum];
+        
+        return memo[i][sum] = Math.min(helper(i, sum - coins[i] ,coins, memo) + 1,  helper(i+1, sum, coins, memo));
     }
 }
