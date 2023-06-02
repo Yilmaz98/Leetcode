4fe41d87e5df9d04ed9f27329ed8c9e86ca0f44f
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-01 21:14:16 -0400

    Time: 0 ms (100.00%), Space: 39.7 MB (33.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,18 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n == 1)
-            return 1;
-        int[] dp = new int[n];
-        
+        int[] dp = new int[n+1];
         dp[0] = 1;
-        dp[1] = 2;
+        dp[1] = 1;
         
-        for(int i=2;i<n;i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
+        return helper(n, dp);
+    }
+    
+    public int helper(int n, int[] dp) {
+        if(dp[n] != 0)
+            return dp[n];
         
-        return dp[n-1];
+        int result = helper(n-1, dp) + helper(n-2,dp);
+        
+        return dp[n] = result;
     }
 }
