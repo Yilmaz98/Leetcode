8348713aed4a2c7c21e8c70174904f08c5b78c7b
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-02 17:32:22 -0400

    Time: 3 ms (61.92%), Space: 40.8 MB (36.65%) - LeetHub

---
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int mctFromLeafValues(int[] arr) {
+        int n = arr.length;
+        int[][] memo = new int[n][n];
+        int[][] maxx = new int[n][n];
+        
+        for(int i=0;i<n;i++) {
+            maxx[i][i] = arr[i]; 
+            for(int j=i+1;j<n;j++) {
+                maxx[i][j] = Math.max(maxx[i][j-1], arr[j]);
+            }
+        }
+        
+        return helper(memo,maxx, arr,0,arr.length-1);
+    
+    }
+    
+    public int helper(int[][] memo, int[][] maxx, int[] arr, int start, int end) {
+        if(start >= end)
+            return 0;
+        
+        if(memo[start][end] != 0)
+            return memo[start][end];
+        
+        int result = Integer.MAX_VALUE;
+        for(int i= start;i<end;i++) {
+            result = Math.min(result, helper(memo, maxx, arr,start,i) + helper(memo,maxx,arr,i+1,end) + (maxx[start][i] * maxx[i+1][end]));
+        }
+        
+        return memo[start][end] = result;
+    }
+    
+}
