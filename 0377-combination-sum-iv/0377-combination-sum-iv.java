10dd631f5beb59ebc4ac1c2d1a08b1630e3a5328
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-02 01:26:29 -0400

    Time: 0 ms (100.00%), Space: 39.9 MB (68.61%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,21 +1,31 @@
 class Solution {
-   public int combinationSum4(int[] nums, int target) {
-        Integer[] memo = new Integer[target + 1];
-        return recurse(nums, target, memo);
+    public int combinationSum4(int[] nums, int target) {
+        Integer[] memo = new Integer[target+1];
+        
+        return helper(memo, nums, target);
+    
     }
-
-    public int recurse(int[] nums, int remain, Integer[] memo) {
-
-        if (remain < 0) return 0;
-        if (memo[remain] != null) return memo[remain];
-        if (remain == 0) return 1;
-
-        int ans = 0;
-        for (int i = 0; i < nums.length; i++) {
-            ans += recurse(nums, remain - nums[i], memo);
+    
+    public int helper(Integer[] memo, int[] nums, int sum) {
+        if(sum == 0) {
+             return 1;
         }
 
-        memo[remain] = ans;
-        return memo[remain];
+        
+        if(sum < 0)
+            return 0;
+        
+        if(memo[sum] !=null) {
+            return  memo[sum];
+        }
+        
+        int ways = 0;
+        for(int j=0;j<nums.length;j++) {
+                ways += helper(memo,nums,sum - nums[j]);
+        }
+        
+        memo[sum] = ways;
+        
+        return memo[sum];
     }
 }
