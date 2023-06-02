a788994bdd7cea2bfc30d0a7b479c37410801d98
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-01 21:25:48 -0400

    Time: 0 ms (100.00%), Space: 39.8 MB (21.10%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -2,14 +2,6 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] memo = new int[m][n];
         helper(memo, m, n, 0, 0);
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                System.out.print(memo[i][j] + " ");
-        }
-        System.out.println();
-        }
-        
-        
         return memo[0][0];
     }
     
