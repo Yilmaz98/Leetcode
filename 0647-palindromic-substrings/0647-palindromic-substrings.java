fd1efd68f4253049e63dbfa2d4baf8102db05206
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-03 01:16:45 -0400

    Time: 4 ms (84.73%), Space: 40.6 MB (82.44%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,18 @@
+class Solution {
+    int count = 1;
+    public int countSubstrings(String s) {
+        for(int i=0;i<s.length()-1;i++) {
+            extendPalindrome(s,i,i);
+            extendPalindrome(s,i,i+1);
+        }
+        return count;
+    }
+    
+    public void extendPalindrome(String s, int j, int k) {
+        while(j>=0 && k<s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+            count++;
+        }
+    }
+}
