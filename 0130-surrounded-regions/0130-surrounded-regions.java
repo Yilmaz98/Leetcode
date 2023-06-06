f73f789524986f439e404b9d06648b5aa9df473e
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-06 10:54:42 -0400

    Time: 14 ms (5.88%), Space: 47.6 MB (8.56%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,17 +1,16 @@
 class Solution {
-    public void dfs(char[][] board, int i, int j, int m, int n, Set<String>visited,Set<String> cells ) {
-        if(i<0 || i>=m || j<0 || j>=n || visited.contains(i+"," + j) || board[i][j] == 'X') {
+    public void dfs(char[][] board, int i, int j, int m, int n,Set<String> cells ) {
+        if(i<0 || i>=m || j<0 || j>=n || cells.contains(i+"," + j) || board[i][j] == 'X') {
             return;
         }
         
-        visited.add(new String(i + "," + j));
         cells.add(new String(i + "," + j));
 
         
-        dfs(board,i+1,j,m,n,visited,cells);
-        dfs(board,i,j+1,m,n,visited,cells);
-        dfs(board,i-1,j,m,n,visited,cells);
-        dfs(board,i,j-1,m,n,visited,cells);
+        dfs(board,i+1,j,m,n,cells);
+        dfs(board,i,j+1,m,n,cells);
+        dfs(board,i-1,j,m,n,cells);
+        dfs(board,i,j-1,m,n,cells);
     }
     
     public void solve(char[][] board) {
@@ -23,7 +22,7 @@ class Solution {
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
                 if((i==0 || i == m-1 || j == 0 || j == n-1) && (board[i][j] == 'O'))  
-                    dfs(board, i, j, m,n,new HashSet<>(), cells);
+                    dfs(board, i, j, m,n, cells);
             }
         }
         
