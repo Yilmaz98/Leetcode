ab8bbdb58adb7419a7c6ec6429ec65dfba38ebc0
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-07 17:46:24 -0400

    Time: 14 ms (28.64%), Space: 45.6 MB (77.81%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,40 +1,45 @@
 class Solution {
-    public int partition(int[] weights, int capacity) {
-        int count = 1;
+    
+    public int capacity(int weight, int[] weights) {
         int sum = 0;
+        int days = 1;
+        
         for(int i=0;i<weights.length;i++) {
-            if(sum > capacity) {
-                count = weights.length;
-                break;
+            if(sum > weight)
+                return weights.length;
+            
+            if(sum + weights[i] > weight) {
+                days++;
+                sum = 0;
             }
                 
-            if(sum + weights[i] > capacity) {
-                count++;
-                sum = 0; 
-            }   
             sum += weights[i];
         }
-        return count;
+        
+        return days;
     }
     
-    public int shipWithinDays(int[] weights, int days) {
-        int sum = 0;
+    public int shipWithinDays(int[] weights, int days) {        
+        int totalWeight = 0;
         int max = 0;
+        
         for(int i=0;i<weights.length;i++) {
-            sum += weights[i];  
+            totalWeight += weights[i];
             max = Math.max(max, weights[i]);
-        }    
+        }
+        
         int low = 0;
-        int high = sum;
+        int high = totalWeight;
         
         while(low < high) {
-            int mid = low + (high-low)/2;  
-            //System.out.println(mid);
-            if(partition(weights,mid) <= days) {
+            int mid = low + (high - low)/2;
+            
+            if(capacity(mid, weights) > days) {
+                low = mid + 1; 
+            } 
+            else {
                 high = mid;
-            } else {
-                  low = mid+1;
-            }            
+            } 
         }
         
         if(low == 0)
