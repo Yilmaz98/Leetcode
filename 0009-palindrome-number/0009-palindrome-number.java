84354c4b1df53a2078489f5122cc1a6fbc249688
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-13 17:02:23 -0400

    Time: 9 ms (99.26%), Space: 42.8 MB (42.56%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,10 +1,9 @@
 class Solution {
   public boolean isPalindrome(int num){
    if(num < 0) return  false; 
-   int reversed = 0, remainder, original = num;
-   while(num != 0) {
-        remainder = num % 10; // reversed integer is stored in variable
-        reversed = reversed * 10 + remainder; //multiply reversed by 10 then add the remainder so it gets stored at next decimal place.
+   int reversed = 0, original = num;
+   while(num != 0) { // reversed integer is stored in variable
+        reversed = reversed * 10 + (num % 10); //multiply reversed by 10 then add the remainder so it gets stored at next decimal place.
         num  /= 10;  //the last digit is removed from num after division by 10.
     }
     // palindrome if original and reversed are equal
