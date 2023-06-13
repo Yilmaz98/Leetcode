a56532d59621ed33ec935fae02ac01e80b638ae0
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-13 16:14:49 -0400

    Time: 9 ms (13.38%), Space: 44.1 MB (8.81%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,36 +1,41 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> m = new HashMap<>();
-         m.put('I',1);
-         m.put('V',5);
-         m.put('X',10);
-         m.put('L',50);
-         m.put('C',100);
-         m.put('D',500);
-         m.put('M',1000);
+        Map<String, Integer> m = new HashMap<>();
+         m.put("I",1);
+         m.put("V",5);
+         m.put("X",10);
+         m.put("L",50);
+         m.put("C",100);
+         m.put("D",500);
+         m.put("M",1000);
+         m.put("CM",900);
+         m.put("XC",90);
+         m.put("IV",4);
+         m.put("IX",9);
+         m.put("XL",40);
+         m.put("CD",400);
         
         if(s.length() == 1)
-            return m.get(s.charAt(0));
+            return m.get(s.substring(0,1));
         
         int result = 0;
         
         for(int i=0;i<s.length() -1;i++) {
-            int curr = m.get(s.charAt(i));
-            int next = m.get(s.charAt(i+1));
-            
-            if(curr < next) {
-                 result += (next - curr); 
-                 i++;
+            int curr = m.get(s.substring(i,i+1));
+
+            if(m.containsKey(s.substring(i,i+2))) {
+                result += m.get(s.substring(i,i+2));
+                i++;
             } else {
                 result += curr;
             }
         }
-        int n = s.length();
-        int curr = m.get(s.charAt(n-2));
-        int next = m.get(s.charAt(n-1));
         
-        if(curr >= next)
-            result +=next;
+        int n = s.length();
+        
+        if(!m.containsKey(s.substring(n-2,n))) {
+            result += m.get(s.substring(n-1,n));
+        }
         
         return result;
     }
