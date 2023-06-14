1ff0930b32e325333d74f7f2694d98f30fe6bb8d
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-14 00:49:13 -0400

    Time: 3 ms (85.20%), Space: 41.8 MB (77.52%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -26,11 +26,20 @@ class Solution {
         }
     }
     
+    public class MyComparator implements Comparator<Node> {
+        public int compare(Node n1, Node n2) {
+            if(n1.row == n2.row)
+                return n1.node.val - n2.node.val;
+            
+            return n1.row - n2.row;
+        }
+    }
+    
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        Queue<Node> q = new LinkedList<>();
+        PriorityQueue<Node> q = new PriorityQueue<>(new MyComparator());
         q.add(new Node(0,0, root));
         
-        TreeMap<Integer, List<Node>> tm = new TreeMap<>();
+        TreeMap<Integer, List<Integer>> tm = new TreeMap<>();
         
         while(!q.isEmpty()) {
             int size = q.size();
@@ -40,7 +49,7 @@ class Solution {
             if(!tm.containsKey(curr.col))
                 tm.put(curr.col, new ArrayList<>());
             
-            tm.get(curr.col).add(curr);
+            tm.get(curr.col).add(curr.node.val);
             
             if(curr.node.left != null) {
                 q.add(new Node(curr.col -1, curr.row + 1, curr.node.left));
@@ -52,27 +61,6 @@ class Solution {
             }
         }
         
-        List<List<Node>> temp = new ArrayList<>(tm.values());
-        
-        List<List<Integer>> result = new ArrayList<>();
-        
-        for(int i=0;i<temp.size();i++) {
-            Collections.sort(temp.get(i), new Comparator<>() {
-                @Override
-                public int compare(Node n1, Node n2) {
-                    if(n1.row == n2.row)
-                        return n1.node.val - n2.node.val;
-                    else 
-                        return n1.row - n2.row;
-                }
-            });
-            List<Integer> finalRow = new ArrayList<>();
-             for(int j=0;j<temp.get(i).size();j++) {
-                finalRow.add(temp.get(i).get(j).node.val);
-             }
-            result.add(finalRow);
-        }
-        
-        return result;
+        return new ArrayList<>(tm.values());
     }
 }
