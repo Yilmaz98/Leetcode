2f92d966b6aa80e7265eb19fbc4b1c11c9ec8970
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-14 16:00:54 -0400

    Time: 365 ms (7.16%), Space: 56.7 MB (55.65%) - LeetHub

---
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] findBuildings(int[] heights) {
+        List<Integer> result = new ArrayList<>();
+        int n = heights.length;
+        int prev = heights[n-1];
+        
+        result.add(0,n-1);
+        for(int i=n-2;i>=0;i--) {
+            if(heights[i] > prev) {
+                result.add(0, i);
+                prev = heights[i];
+            }
+        }
+        
+        int[] ans = new int[result.size()];
+        
+        for(int i=0;i<result.size();i++) {
+            ans[i] = result.get(i);
+        }
+        
+        return ans;
+    }
+}
