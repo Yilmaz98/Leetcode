29515cc5b230ee686954a2fb3093965081a3560a
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-14 19:52:22 -0400

    Time: 15 ms (18.22%), Space: 57.8 MB (9.53%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
@@ -1,24 +1,33 @@
 class SparseVector {
-    Map<Integer, Integer > m;
+    List<int[]> idxList;
+    
     SparseVector(int[] nums) {
-        m = new HashMap<>();
+        idxList = new ArrayList<>();
         
         for(int i=0;i<nums.length;i++) {
-            if(0 != nums[i]) {
-                m.put(i, nums[i]);
-            }
+            if(nums[i] != 0)
+                idxList.add(new int[]{i, nums[i]});
         }
     }
     
 	// Return the dotProduct of two sparse vectors
     public int dotProduct(SparseVector vec) {
-        int result = 0;
         
-        for(Integer i: m.keySet()) {
-            if(vec.m.containsKey(i)) {
-                result += m.get(i) * vec.m.get(i);
+        int result = 0;
+        int p = 0;
+        int q = 0;
+           
+        while(p < idxList.size() && q < vec.idxList.size()) {
+            if(idxList.get(p)[0] == vec.idxList.get(q)[0]) {
+                result += idxList.get(p)[1] * vec.idxList.get(q)[1];
+                p++;
+                q++;
+            } else if(idxList.get(p)[0] > vec.idxList.get(q)[0]) {
+                q++;
+            } else {
+                p++;
             }
-        }
+         }
         
         return result;
     }
