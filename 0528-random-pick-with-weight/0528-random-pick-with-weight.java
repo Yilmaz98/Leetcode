77a58bc21af3133ed249e04ba413eeba57c047eb
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-15 02:56:27 -0400

    Time: 30 ms (90.14%), Space: 47.6 MB (46.25%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.java
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.java
@@ -1,40 +1,31 @@
 class Solution {
-    int[] prefixSums;
-    int totalSum;
-    
+    private int[] prefixSums;
+    private int totalSum;
+
     public Solution(int[] w) {
-        prefixSums = new int[w.length];
-        
-        int sum = 0;
-        for(int i=0;i<w.length;i++) {
-            sum+=w[i];
-            prefixSums[i] = sum;
+        this.prefixSums = new int[w.length];
+
+        int prefixSum = 0;
+        for (int i = 0; i < w.length; ++i) {
+            prefixSum += w[i];
+            this.prefixSums[i] = prefixSum;
         }
-        totalSum = sum;
+        this.totalSum = prefixSum;
     }
-    
+
     public int pickIndex() {
-        double target = totalSum * Math.random();
-        
-        int low = 0;
-        int high = prefixSums.length -1;
-        
-        while(low<high) {
-            int mid = low + (high-low)/2;
-            
-            if(prefixSums[mid] < target) {
-                low = mid+1;
-            } else {
+        double target = this.totalSum * Math.random();
+
+        // run a binary search to find the target zone
+        int low = 0, high = this.prefixSums.length;
+        while (low < high) {
+            // better to avoid the overflow
+            int mid = low + (high - low) / 2;
+            if (target > this.prefixSums[mid])
+                low = mid + 1;
+            else
                 high = mid;
-            }
         }
-        
         return low;
     }
 }
-
-/**
- * Your Solution object will be instantiated and called as such:
- * Solution obj = new Solution(w);
- * int param_1 = obj.pickIndex();
- */
