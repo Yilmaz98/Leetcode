70cc9e619cff3b46f2f3f5b951bfafa0eeaf23e4
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-18 11:48:09 -0700

    Time: 14 ms (54.55%), Space: 44.8 MB (9.09%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
@@ -1,26 +1,23 @@
 class Solution {
-    public int[] exclusiveTime(int n, List<String> logs) {
-           int[] res = new int[n];
-        int ptime = 0, running = 0;
-        Stack<Integer> stack = new Stack<>();
+   public int[] exclusiveTime(int n, List<String> logs) {
+		int[] res = new int[n];
+		Deque<Integer> stack = new LinkedList<>();
+		int prevTime = 0;
 
-        for (String log : logs) {
-            String[] split = log.split(":");
-            int func = Integer.parseInt(split[0]);
-            boolean start = split[1].equals("start");
-            int time = Integer.parseInt(split[2]);
-            if (!start)
-                time++;
+		for (String log : logs) {
+			String[] split = log.split(":");
+			int funcId = Integer.parseInt(split[0]);
+			boolean isStart = split[1].equals("start");
+			int time = Integer.parseInt(split[2]);
+			if (!isStart) time++;
 
-            res[running] += (time - ptime);
-            if (start) {
-                stack.push(running);
-                running = func;
-            } else {
-                running = stack.pop();
-            }
-            ptime = time;
-        }
-        return res;
-        }
+			if (!stack.isEmpty()) {
+				res[stack.peek()] += time - prevTime;
+			}
+			if (isStart) stack.push(funcId);
+			else stack.pop();
+			prevTime = time;
+		}
+		return res;
+	}
 }
