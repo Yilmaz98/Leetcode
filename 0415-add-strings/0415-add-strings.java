a0707b1bd599caee01b89eb5fc13da3f52800040
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-20 11:50:03 -0700

    Time: 4 ms (42.17%), Space: 43.9 MB (17.06%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,23 +1,44 @@
 class Solution {
-    public String addStrings(String num1, String num2) {
-        StringBuilder res = new StringBuilder();
-
-        int carry = 0;
-        int p1 = num1.length() - 1;
-        int p2 = num2.length() - 1;
-        while (p1 >= 0 || p2 >= 0) {
-            int x1 = p1 >= 0 ? num1.charAt(p1) - '0' : 0;
-            int x2 = p2 >= 0 ? num2.charAt(p2) - '0' : 0;
-            int value = (x1 + x2 + carry) % 10;
-            carry = (x1 + x2 + carry) / 10;
-            res.append(value);
-            p1--;
-            p2--;    
+    public String addStrings(String num1, String num2) {        
+        if(num1.length() > num2.length()) {
+            return addStrings(num2,num1);
         }
         
-        if (carry != 0)
-            res.append(carry);
+        int m = num1.length();
+        int n = num2.length();
+        int j = m - 1;
+        int i = n - 1;
         
-        return res.reverse().toString();
+
+        int carry = 0;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(j >= 0) {
+            int val1 = Integer.parseInt(String.valueOf(num1.charAt(j)));
+            int val2 = Integer.parseInt(String.valueOf(num2.charAt(i)));
+            
+            int sum = (val1 + val2 + carry);
+            
+            sb.append(sum % 10);
+            
+            carry = sum/10;
+            
+            j--;
+            i--;
+        }
+        
+        while(i >=0) {
+            int val1 = Integer.parseInt(String.valueOf(num2.charAt(i)));
+            int sum =  (val1 + carry);
+            sb.append(sum % 10);
+            carry = sum/10;
+            i--;
+        }
+        
+        if(carry > 0)
+            sb.append(carry);
+        
+        return sb.reverse().toString();
     }
 }
