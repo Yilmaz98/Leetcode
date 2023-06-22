c20da04f7a65b760713b92b3aba1566cec62d0a8
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-22 13:27:35 -0700

    Time: 4 ms (49.06%), Space: 44.8 MB (27.61%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -1,24 +1,31 @@
 class Solution {
-  public int[][] intervalIntersection(int[][] A, int[][] B) {
-    List<int[]> ans = new ArrayList();
-    int i = 0, j = 0;
-
-    while (i < A.length && j < B.length) {
-      // Let's check if A[i] intersects B[j].
-      // lo - the startpoint of the intersection
-      // hi - the endpoint of the intersection
-      int lo = Math.max(A[i][0], B[j][0]);
-      int hi = Math.min(A[i][1], B[j][1]);
-      if (lo <= hi)
-        ans.add(new int[]{lo, hi});
-
-      // Remove the interval with the smallest endpoint
-      if (A[i][1] < B[j][1])
-        i++;
-      else
-        j++;
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        int i = 0;
+        int j = 0;
+        
+        List<int[]> ans = new ArrayList<>();
+        
+        while(i < firstList.length && j < secondList.length) {
+            if(firstList[i][1] >= secondList[j][0]) {
+                int lo = Math.max(firstList[i][0], secondList[j][0]);
+                int hi = Math.min(firstList[i][1], secondList[j][1]);
+                if(lo <= hi)
+                    ans.add(new int[] {lo, hi});
+            }
+             if(firstList[i][1] < secondList[j][1]) {
+            i++;
+        } else {
+            j++;
+        }
+        }
+        
+        int[][] res = new int[ans.size()][2];
+        
+        for(i=0;i<ans.size();i++) {
+            
+            res[i] = ans.get(i);
+        }
+       
+        return res;
     }
-
-    return ans.toArray(new int[ans.size()][]);
-  }
 }
