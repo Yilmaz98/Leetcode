71c1cbbddaed164dad25a1a478736a1c76c1fc21
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-22 12:32:02 -0700

    Time: 41 ms (41.53%), Space: 57.5 MB (11.48%) - LeetHub

--- 1891-cutting-ribbons/1891-cutting-ribbons.java
+++ 1891-cutting-ribbons/1891-cutting-ribbons.java
@@ -1,23 +1,32 @@
 class Solution {
-    public int maxLength(int[] ribbons, int k) {
-        int max = Integer.MIN_VALUE;
-        for(int len : ribbons)
-            max = Math.max(max, len);
+    public long helper(int[] ribbons, int val) {
+        long count = 0;
         
-        int n = ribbons.length, low = 1, high = max, mid, count;        
-        while(low <= high) {
-            mid = low + (high - low)/2;
-            
-            count = 0;
-            for(int length : ribbons)
-                count += length/mid;
-                        
-            if(count < k) 
-                high = mid-1;
-            else 
-                low = mid+1;
+        for(int i=0;i<ribbons.length;i++) {
+            count += (ribbons[i]/val);
         }
-                    
+        
+        return count;
+    } 
+    
+    public int maxLength(int[] ribbons, int k) {
+        int low = 1;
+        int high = 0;
+        
+        for(int i=0;i<ribbons.length;i++) {
+            high = Math.max(high,ribbons[i]);
+        }
+        
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            
+            if(helper(ribbons, mid) < k) {
+                 high = mid -1;
+            } else {
+                 low = mid + 1;       
+            }
+        }
+        
         return high > 0 ? high : 0;
-    }    
+    }
 }
