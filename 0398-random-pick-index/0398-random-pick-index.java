01573191c064b97e82ec8b0eb57f7cb27ca2edf5
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-22 10:37:50 -0700

    Time: 2736 ms (5.01%), Space: 55.9 MB (11.84%) - LeetHub

--- 0398-random-pick-index/0398-random-pick-index.java
+++ 0398-random-pick-index/0398-random-pick-index.java
@@ -1,24 +1,30 @@
 class Solution {
 
-    private HashMap<Integer, List<Integer>> indices;
+    private int[] nums;
     private Random rand;
     
     public Solution(int[] nums) {
+        this.nums = nums;
         this.rand = new Random();
-        this.indices = new HashMap<Integer, List<Integer>>();
-        int l = nums.length;
-        for (int i = 0; i < l; ++i) {
-            if (!this.indices.containsKey(nums[i])) {
-                this.indices.put(nums[i], new ArrayList<>());
-            }
-            this.indices.get(nums[i]).add(i);
-        }
     }
     
     public int pick(int target) {
-        int l = indices.get(target).size();
-        // pick an index at random
-        int randomIndex = indices.get(target).get(rand.nextInt(l));
-        return randomIndex;
+        int n = this.nums.length;
+        int count = 0;
+        int idx = 0;
+        for (int i = 0; i < n; ++i) {
+            // if nums[i] is equal to target, i is a potential candidate
+            // which needs to be chosen uniformly at random
+            if (this.nums[i] == target) {
+                // increment the count of total candidates
+                // available to be chosen uniformly at random
+                count++;
+                // we pick the current number with probability 1 / count (reservoir sampling)
+                if (rand.nextInt(count) == 0) {
+                    idx = i;
+                }
+            }
+        }
+        return idx;
     }
 }
