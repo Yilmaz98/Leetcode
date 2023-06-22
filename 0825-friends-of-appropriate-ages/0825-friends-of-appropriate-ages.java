04083458fd3b8bd59a8315be236de540bfa05f84
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-22 14:17:41 -0700

    Time: 24 ms (30.88%), Space: 47.9 MB (5.15%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
@@ -1,24 +1,16 @@
 class Solution {
-    public int numFriendRequests(int[] ages) {
+       public int numFriendRequests(int[] ages) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int age : ages)
+            count.put(age, count.getOrDefault(age, 0) + 1);
         int res = 0;
-        Arrays.sort(ages);
-        for (int i = 0; i < ages.length; ++i) {
-            int age = ages[i];
-            int lower = firstIdx(ages, age/2+7);
-            int upper = firstIdx(ages, age);
-            res += Math.max(upper-lower-1, 0);
-        }
+        for (Integer a : count.keySet())
+            for (Integer b : count.keySet())
+                if (request(a, b)) res += count.get(a) * (count.get(b) - (a == b ? 1 : 0));
         return res;
     }
-    
-    private static int firstIdx(int[] ages, int target) {
-        int beg = 0;
-        int end = ages.length-1;
-        while (beg <= end) {
-            int mid = beg + (end-beg)/2;
-            if (ages[mid] <= target) beg = mid + 1;
-            else end = mid - 1;
-        }
-        return beg;
+
+    private boolean request(int a, int b) {
+        return !(b <= 0.5 * a + 7 || b > a || (b > 100 && a < 100));
     }
 }
