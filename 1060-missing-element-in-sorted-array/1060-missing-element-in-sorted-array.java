30915154a696b243f28bfa8c2454b336f4451e1f
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-06-23 22:17:06 -0700

    Time: 2 ms (18.37%), Space: 47.1 MB (79.22%) - LeetHub

--- 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.java
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.java
@@ -1,13 +1,16 @@
 class Solution {
     public int missingElement(int[] nums, int k) {
+        
+        
         for(int i=0;i<nums.length -1;i++) {
-            if(k < nums[i+1] - nums[i]) {
-                return nums[i] + k;        
-            } else {
-                k = k- (nums[i+1] - nums[i] - 1);
-            }
+            int remainingNums = nums[i+1] - nums[i] -1;
+            if(remainingNums >= k) {
+                return nums[i] + k;
+            } 
+            if(remainingNums > 0)
+                k-=remainingNums;
         }
         
-        return nums[nums.length-1] + k;
+        return nums[nums.length -1] + k;
     }
 }
