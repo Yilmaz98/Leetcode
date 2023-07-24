40f78f22af252ef1b92e0a72905e6580ab65a635
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-07-23 17:14:34 -0700

    Time: 7 ms (92.60%), Space: 43.6 MB (74.94%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,43 +1,58 @@
 class Solution {
-    int dirX[] = {-1,1,0,0};
-    int dirY[] = {0,0,-1,1};
+    int[][] dirs = {{-1,0},{0,1},{0,-1},{1,0}};
+    boolean isValid(int x, int y, int m, int n) {
+        if(x>=0 && x<m && y>=0 && y<n)
+            return true;
+        return false;
+    }
     
-    public int bfs(int[][] grid, int m, int n, int k) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0,0,k});
-        boolean[][][] visited = new boolean[m][n][k+1];
-        int steps = 0;
-        while(!q.isEmpty()) {
-        int size = q.size();
-        for(int j=0;j<size;j++) {
-            int[] curr = q.poll();
-          if(curr[0] == m-1 && curr[1] == n-1)
-              return steps;
-            
-            for(int i=0;i<4;i++) {
-              int newX = curr[0] + dirX[i];
-              int newY = curr[1] + dirY[i];
-              int obs = curr[2];                
-             if(newX>=0 && newX < m && newY >= 0 && newY< n && obs>=0 && !visited[newX][newY][obs]) {
-                  visited[newX][newY][obs] = true;
-                 if(grid[newX][newY] == 1) {
-                    q.add(new int[]{newX,newY, obs - 1}); 
-                 } else {
-                     q.add(new int[]{newX,newY,obs}); 
-                 } 
-             }
-            }
-                    
-          }
-            steps++;   
-        }
-        return -1;
+    boolean isObstacle(int x, int y, int[][] grid) {
+        return grid[x][y] == 1;
     }
     
     public int shortestPath(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
+        if(k > m+n-2)
+            return m+n-2;
         
-        return bfs(grid, m,n,k);
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0,k});
+        boolean[][][] visited = new boolean[m][n][k+1];
+        
+        int steps = 0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i=0;i<size;i++) {        
+                int[] curr = q.poll();
+                
+                
+                if(curr[0] == m-1 && curr[1] == n-1) {
+                    return steps;
+                }
+                
+                for(int[] dir : dirs) {
+                    int newX = curr[0] + dir[0];
+                    int newY = curr[1] + dir[1];
+                    int currK = curr[2];
+                    
+                    if(isValid(newX, newY,m,n) && !visited[newX][newY][currK]) {
+                        if(isObstacle(newX,newY,grid)) {
+                            if(currK-1<0)
+                                continue;
+                            visited[newX][newY][currK] = true;
+                            q.add(new int[]{newX,newY,currK-1});
+                        } else{
+                            visited[newX][newY][currK] = true;
+                            q.add(new int[]{newX,newY,currK});
+                        }
+                    }
+                }
+            }
+            steps++;
+        }
+        
+        return -1;
     }
 }
