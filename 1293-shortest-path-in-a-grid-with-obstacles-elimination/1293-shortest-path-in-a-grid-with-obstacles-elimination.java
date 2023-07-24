fb698f0bb907181370eb2e34c9d88708bc2deddf
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-07-23 21:42:41 -0700

    Time: 5 ms (95.11%), Space: 41 MB (99.28%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -6,19 +6,21 @@ class Solution {
         return false;
     }
     
-    boolean isObstacle(int x, int y, int[][] grid) {
-        return grid[x][y] == 1;
-    }
-    
     public int shortestPath(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-        if(k > m+n-2)
+        if(k >= m+n-2)
             return m+n-2;
         
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{0,0,k});
-        boolean[][][] visited = new boolean[m][n][k+1];
+        int[][] visited = new int[m][n];
+        
+        for(int[] curr : visited){
+            Arrays.fill(curr,-1);
+        }
+        
+        visited[0][0] = k;
         
         int steps = 0;
         while(!q.isEmpty()) {
@@ -27,26 +29,19 @@ class Solution {
             for(int i=0;i<size;i++) {        
                 int[] curr = q.poll();
                 
-                
                 if(curr[0] == m-1 && curr[1] == n-1) {
                     return steps;
                 }
                 
                 for(int[] dir : dirs) {
                     int newX = curr[0] + dir[0];
-                    int newY = curr[1] + dir[1];
-                    int currK = curr[2];
-                    
-                    if(isValid(newX, newY,m,n) && !visited[newX][newY][currK]) {
-                        if(isObstacle(newX,newY,grid)) {
-                            if(currK-1<0)
+                    int newY = curr[1] + dir[1];                    
+                    if(isValid(newX, newY,m,n)) {
+                            int newK = grid[newX][newY] == 1 ? curr[2] - 1: curr[2];
+                            if(newK <= visited[newX][newY])
                                 continue;
-                            visited[newX][newY][currK] = true;
-                            q.add(new int[]{newX,newY,currK-1});
-                        } else{
-                            visited[newX][newY][currK] = true;
-                            q.add(new int[]{newX,newY,currK});
-                        }
+                            visited[newX][newY] = newK;
+                            q.add(new int[]{newX,newY, newK});
                     }
                 }
             }
