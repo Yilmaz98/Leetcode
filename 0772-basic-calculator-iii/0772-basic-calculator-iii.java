19c54f437c95cbf6e1166aae5ba178f2736a4cb7
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-07-23 19:05:21 -0700

    Time: 8 ms (31.18%), Space: 40.5 MB (95.31%) - LeetHub

--- 0772-basic-calculator-iii/0772-basic-calculator-iii.java
+++ 0772-basic-calculator-iii/0772-basic-calculator-iii.java
@@ -1,63 +1,85 @@
+/*
+We evaluate expressions from the left to the right. When we see two adjacent operators,
+we can't do the first operation directly unless the precedence of the second operation is equal to
+or less than the first operator.
+*/
+
 class Solution {
+    Map<Character, Integer> precedence;
+    
     public int calculate(String s) {
-        if(s.length() == 0 || s == null) return 0;
+        if (s == null || s.length() == 0) return 0;
         
-        Stack<Integer> nums = new Stack<>();
+        precedence = new HashMap<>();
+        precedence.put('+', 1);
+        precedence.put('-', 1);
+        precedence.put('*', 2);
+        precedence.put('/', 2);
+        precedence.put('(', 0); // !!
+        
+        return evalExpression(s);
+    }
+                       
+    private int evalExpression(String s) {
+        // Assert s is valid  
         Stack<Character> ops = new Stack<>();
-        int operand = 0;
+        Stack<Integer> values = new Stack<>();
         
-        for(int i=0;i<s.length();i++) {
-            char ch = s.charAt(i);
-            if(Character.isDigit(ch)) {
-                operand = (ch - '0');
-                while(i + 1< s.length() && Character.isDigit(s.charAt(i+1))) {
-                    operand = (operand*10) + (s.charAt(i+1) - '0');
-                    i++;
+        char[] tokens = s.toCharArray();
+        char pre = ' ';
+        for (int i = 0; i < tokens.length; i++) {
+            char c = tokens[i];
+            if (c == ' ') {
+                continue;
+            }
+            
+            if (Character.isDigit(c)) {
+                int num = 0;
+                while (i < tokens.length && Character.isDigit(tokens[i]))
+                     num = 10*num + tokens[i++] - '0';
+                values.push(num);
+                i--;
+            } else if (c == '(') {
+                ops.push(c);
+            } else if (c == ')') {
+                while (ops.peek() != '(')
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                ops.pop();  // pop out '('
+            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+                // Dealing with the negative number
+                if (i == 0 && c == '-') {
+                    values.push(0);
+                } else if (pre == '(' && c == '-') {
+                    values.push(0);
                 }
-                nums.push(operand);
-                operand = 0;
-            } 
-            else if(ch == '(') {
-                ops.push(ch);
-            } else if(ch == ')') {
-                while(ops.peek() != '(') {
-                    nums.push(operation(nums.pop(), ops.pop(), nums.pop()));
-                }
-                ops.pop();
-            } else if(ch == '+' || ch == '-' || ch == '*' || ch =='/') {
-                while(!ops.isEmpty() && hasPrecedence(ops.peek(), ch)) {
-                    nums.push(operation(nums.pop(), ops.pop(), nums.pop()));
-                }
-                ops.push(ch);
+                
+                // While top of 'ops' has same or greater precedence to current 
+                // token, which is an operator. Apply operator on top of 'ops' 
+                // to top two elements in values stack 
+                while (!ops.isEmpty() && precedence.get(c) <= precedence.get(ops.peek())) 
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+
+                ops.push(c);
+            }
+            pre = c;
+        }
+        
+        while (!ops.isEmpty()) 
+             values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+        
+       return values.pop();
+    }
+                       
+    private int applyOp(Character op, int b, int a) { // b is before a (!!)
+        switch(op) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+            case '/': {
+                assert b != 0;
+                return a / b;
             }
         }
-        
-        while(!ops.isEmpty())
-        {
-            nums.push(operation(nums.pop(), ops.pop(), nums.pop()));
-        }
-        
-        return nums.pop();
-    }
-    
-    public int operation(int a, char op, int b) {
-        switch(op) {
-            case '+': return b + a;
-            case '-': return b - a;
-            case '*': return b * a;
-            case '/': return b / a;
-        }
-        return 0;
-    }
-    
-    public boolean hasPrecedence(char op1, char op2) {
-        if(op1 == '(' )
-            return false;
-        
-        if((op1 == '+' || op1 == '-') && (op2 == '*' || op2 == '/')) {
-            return false;
-        }
-        
-        return true;
+        return Integer.MIN_VALUE;
     }
 }
