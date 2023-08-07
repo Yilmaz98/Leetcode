fde969af7fdc06a74234b84c988d0ae0ba8921c1
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-08-06 17:18:18 -0700

    Time: 1 ms (93.78%), Space: 41 MB (44.81%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,20 +1,22 @@
-class Solution {
-      public int numDecodings(String s) {
-        HashMap<String, Integer> cache = new HashMap<>();
-        return numDecodingsHelper(s, cache);
+public class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
         }
-    
-    public int numDecodingsHelper(String s, HashMap<String, Integer> cache){
-        if(cache.containsKey(s)) return cache.get(s);
-        if(s==null || s.length()==0) return 1;
-        int count = 0;
-        for(int i=1;i<=s.length();i++){
-            String temp = s.substring(0,i);
-            if(temp.charAt(0)!='0' && Double.parseDouble(temp) >= 1 && Double.parseDouble(temp)<= 26){
-                count += numDecodingsHelper(s.substring(i), cache);
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= n; i++) {
+            int first = Integer.valueOf(s.substring(i - 1, i));
+            int second = Integer.valueOf(s.substring(i - 2, i));
+            if (first >= 1 && first <= 9) {
+               dp[i] += dp[i-1];  
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i-2];
             }
         }
-        cache.put(s,count);
-        return count;
-        }
+        return dp[n];
+    }
 }
