ab3ec41d15de936d89a6e84c4018081c8257c1d8
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-08-12 02:19:41 -0700

    Time: 4 ms (69.98%), Space: 43.5 MB (45.40%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.java
+++ 0799-champagne-tower/0799-champagne-tower.java
@@ -1,18 +1,20 @@
 class Solution {
     public double champagneTower(int poured, int query_row, int query_glass) {
-        double[][] arr = new double[query_row+2][query_row+2];
-        arr[0][0] = (double) poured;
+        double[][] A = new double[102][102];
+        A[0][0] = (double) poured;
+        
         for(int i=0;i<=query_row;i++) {
             for(int j=0;j<=i;j++) {
-                double excess = (arr[i][j] - 1)/2.0;
+                double next = (A[i][j] - 1.0)/2.0;
                 
-                if(excess>0) {
-                    arr[i+1][j] += excess;
-                    arr[i+1][j+1] += excess; 
+                if(next > 0) {
+                     A[i+1][j] += next;
+                A[i+1][j+1] += next;
                 }
+               
             }
         }
-
-        return Math.min(1,arr[query_row][query_glass]);
+        
+        return Math.min(1.0, A[query_row][query_glass]);
     }
 }
