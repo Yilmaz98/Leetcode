f161e71b3b71d97e10f9d9f03c328b298a69fa05
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-08-12 14:17:59 -0700

    Time: 1 ms (64.05%), Space: 40.9 MB (5.14%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.java
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -1,18 +1,21 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-            Stack<Integer> stack = new Stack();
-    stack.push(0); // The score of the current frame
-
-    for (char c: s.toCharArray()) {
-        if (c == '(')
-            stack.push(0);
-        else {
-            int v = stack.pop();
-            int w = stack.pop();
-            stack.push(w + Math.max(2 * v, 1));
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i) == '(') {
+                st.push(0);
+            } else {
+                int u = st.pop();
+                int v = st.pop();
+                
+                st.push(v + Math.max(2 * u, 1));
+            }    
+            
+            //System.out.println(st);
         }
-    }
-
-    return stack.pop();
+        
+        return st.pop();
     }
 }
