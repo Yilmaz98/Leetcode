1f36ee7125cd58835d817e544a48196d8b0a0112
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-08-12 14:06:34 -0700

    Time: 2 ms (70.85%), Space: 41.2 MB (12.14%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,19 +1,15 @@
 class Solution {
-    public class ArrayComparator implements Comparator<int[]> {
-        public int compare(int[] a, int [] b){
-            return (a[0] - a[1]) - (b[0] - b[1]);
-        }
-    }
     public int twoCitySchedCost(int[][] costs) {
-        Arrays.sort(costs, new ArrayComparator());
+        Arrays.sort(costs, (a,b) -> (a[0]-a[1]) - (b[0]-b[1]));
         
-        int ans = 0;
+        int result = 0;
         int n = costs.length/2;
         
         for(int i=0;i<n;i++) {
-            ans += costs[i][0] + costs[i+n][1];
+            result += costs[i][0];
+            result += costs[i+n][1];
         }
         
-        return ans;
+        return result;
     }
 }
