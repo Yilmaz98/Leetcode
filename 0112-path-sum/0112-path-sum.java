e2077dedfc3295d1790a5617fe657b276a24e66f
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-08-13 13:30:55 -0700

    Time: 0 ms (100.00%), Space: 43.2 MB (82.18%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,41 +14,19 @@
  * }
  */
 class Solution {
-    class Node {
-        TreeNode node;
-        int sum;
-        
-        Node(TreeNode node, int sum) {
-            this.node = node;
-            this.sum = sum;
-        }
-    }
     public boolean hasPathSum(TreeNode root, int targetSum) {
+        return dfs(root,0,targetSum);
+    }
+    
+    public boolean dfs(TreeNode root, int sum, int targetSum) {
         if(root == null)
             return false;
         
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(root,0));
+        sum += root.val;
+        if(root.left == null && root.right == null) {
+            return sum == targetSum;
+        } 
         
-        while(!q.isEmpty()) {
-            Node curr = q.poll();
-            
-            curr.sum += curr.node.val;
-            
-            if(curr.node.left == null && curr.node.right == null) {
-                if(curr.sum == targetSum)
-                    return true;
-            }
-                    
-            if(curr.node.left != null) {
-                q.add(new Node(curr.node.left, curr.sum));
-            }
-            
-              if(curr.node.right != null) {
-                q.add(new Node(curr.node.right, curr.sum));
-            }
-        }
-        
-        return false;
-    }
+        return dfs(root.left, sum, targetSum) || dfs(root.right, sum, targetSum);
+    } 
 }
