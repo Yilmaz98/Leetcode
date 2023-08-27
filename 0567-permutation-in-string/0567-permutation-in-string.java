b8e4dd16915cd0980612d4dd2745575d2e64411e
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-08-27 12:35:35 -0700

    Time: 816 ms (10.36%), Space: 44 MB (25.11%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,19 +1,33 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        String sortedS1= sort(s1);
         
-        for(int i=0;i<=s2.length() - s1.length();i++) {
-            String sortedS2 = sort(s2.substring(i, i+ s1.length()));
-           if(sortedS1.equals(sortedS2))
-               return true;
+          if (s1.length() > s2.length())
+            return false;
+        Map<Character, Integer> m1 = new HashMap<>();
+        
+        for(char c : s1.toCharArray()) {
+            m1.put(c, m1.getOrDefault(c,0) + 1);
+        }
+        
+        for(int i = 0;i <= s2.length() - s1.length();i++) {
+            Map<Character, Integer> m2 = new HashMap<>();
+            for(int j=0;j<s1.length();j++) {
+                 m2.put(s2.charAt(i+j), m2.getOrDefault(s2.charAt(i+j),0) + 1);
+            }
+            if(matches(m1,m2)) {
+                return true;
+            }
         }
         
         return false;
     }
     
-    public String sort(String s) {
-        char[] s1String = s.toCharArray();
-        Arrays.sort(s1String);
-        return new String(s1String);
+    public boolean matches(Map<Character, Integer> m1, Map<Character, Integer> m2) {
+        for(Character c : m1.keySet()) {
+            if(m1.get(c) - m2.getOrDefault(c,-1) != 0)
+                return false;
+        }
+        
+        return true;
     }
 }
