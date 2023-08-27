39d414873284054e3c4586b2871e5b74857a0f2d
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-08-26 20:53:33 -0700

    Time: 788 ms (12.88%), Space: 44.6 MB (7.35%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,36 +1,16 @@
-class Solution {
+public class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int len1 = s1.length(), len2 = s2.length();
-        
-        if (len1 > len2) return false;
-        
-        int[] count = new int[26];
-        
-        for (int i = 0; i < len1; i++) {
-            count[s1.charAt(i) - 'a']++;
-            count[s2.charAt(i) - 'a']--;
+        s1 = sort(s1);
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            if (s1.equals(sort(s2.substring(i, i + s1.length()))))
+                return true;
         }
-        
-        if(allZero(count)) {
-            return true;
-        }
-        
-        for (int i = len1; i < len2; i++) {
-            count[s2.charAt(i) - 'a']--;
-            count[s2.charAt(i - len1) - 'a']++;
-            
-            if(allZero(count)) {
-            return true;
-            }
-        }
-                
-        return false;   
+        return false;
     }
     
-        private boolean allZero(int[] count) {
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) return false;
-        }
-        return true;
-        }
+    public String sort(String s) {
+        char[] t = s.toCharArray();
+        Arrays.sort(t);
+        return new String(t);
+    }
 }
