906f00bea8bec8722042a35ac0c4ae631346ecab
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-09-13 22:26:11 -0700

    Time: 0 ms (100.00%), Space: 43.7 MB (78.35%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,53 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
-public class QNode {
-    TreeNode node;
-    long left;
-    long right;
-    QNode(TreeNode node, long left, long right) {
-        this.node = node;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class Solution {
+    // We use Integer instead of int as it supports a null value.
+    private Integer prev;
+
     public boolean isValidBST(TreeNode root) {
-        Queue<QNode> q = new LinkedList<>();
-        q.offer(new QNode(root,Long.MIN_VALUE, Long.MAX_VALUE));
-        
-        while(!q.isEmpty()) {
-            int size = q.size();
-            for(int i=0;i<size;i++) {
-                QNode curr = q.poll();
-                TreeNode n = curr.node;
-                
-                if(n.val >= curr.right || n.val <=curr.left) return false;
-                
-                if(n.left != null) {
-                    q.offer(new QNode(n.left,curr.left,n.val));
-                }
-                
-                if(n.right != null) {
-                    q.offer(new QNode(n.right,n.val,curr.right));
-                }
-            } 
+        prev = null;
+        return inorder(root);
+    }
+
+    private boolean inorder(TreeNode root) {
+        if (root == null) {
+            return true;
         }
-        
+        if (!inorder(root.left)) {
+            return false;
+        }
+        if (prev != null && root.val <= prev) {
+            return false;
+        }
+        prev = root.val;
+        if(!inorder(root.right))
+            return false;
         return true;
     }
 }
