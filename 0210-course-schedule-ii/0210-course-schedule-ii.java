8a59483d3c591f71f00d6689f78b89263e65a429
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-09-14 18:58:51 -0700

    Time: 7 ms (42.98%), Space: 44.3 MB (88.76%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,34 +1,47 @@
 class Solution {
-    public int[] findOrder(int numCourses, int[][] prerequisites) { 
-    if (numCourses == 0) return null;
-    // Convert graph presentation from edges to indegree of adjacent list.
-    int indegree[] = new int[numCourses], order[] = new int[numCourses], index = 0;
-    for (int i = 0; i < prerequisites.length; i++) 
-        indegree[prerequisites[i][0]]++;    
-
-    Queue<Integer> queue = new LinkedList<Integer>();
-    for (int i = 0; i < numCourses; i++) 
-        if (indegree[i] == 0) {
-            // Add the course to the order because it has no prerequisites.
-            order[index++] = i;
-            queue.offer(i);
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        
+        for(int[] prereq : prerequisites) {
+            indegree[prereq[0]]++;
         }
-
-    // How many courses don't need prerequisites. 
-    while (!queue.isEmpty()) {
-        int prerequisite = queue.poll(); // Already finished this prerequisite course.
-        for (int i = 0; i < prerequisites.length; i++)  {
-            if (prerequisites[i][1] == prerequisite) {
-                indegree[prerequisites[i][0]]--; 
-                if (indegree[prerequisites[i][0]] == 0) {
-                    // If indegree is zero, then add the course to the order.
-                    order[index++] = prerequisites[i][0];
-                    queue.offer(prerequisites[i][0]);
-                }
-            } 
+        
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        
+        for(int[] curr : prerequisites) {
+            if(!adj.containsKey(curr[1])) {
+                adj.put(curr[1], new ArrayList<>());
+            }
+            
+            adj.get(curr[1]).add(curr[0]);
+         }
+        
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i=0;i<indegree.length;i++) {
+            if(indegree[i] == 0) {
+                 q.add(i);
+            }
         }
+        
+        int[] order = new int[numCourses];
+        int index = 0;
+        
+        while(!q.isEmpty()) {
+            Integer curr = q.poll();
+            order[index++] = curr;
+            
+            if(!adj.containsKey(curr))
+                continue;
+            
+            for(Integer nei : adj.get(curr)) {
+                if(--indegree[nei] == 0)
+                    q.add(nei);
+            }
+        }
+        
+        if(index != numCourses)
+            return new int[0];
+        return order;
     }
-
-    return (index == numCourses) ? order : new int[0];
-}
 }
