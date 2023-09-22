c8e47f06e0dd6af06c382b17fdeb6b8ee15a541a
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-09-22 12:22:36 -0700

    Time: 1 ms (93.49%), Space: 43.7 MB (80.66%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,25 +1,30 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        helper(nums, new ArrayList<>(), result, new HashSet<>());
+        helper(nums, result, 0);
         return result;
     }
     
-    public void helper(int[] nums, List<Integer> tempList, List<List<Integer>> result, Set<Integer> tempSet) {
-        if(tempList.size() == nums.length)
-            result.add(new ArrayList<>(tempList));
+    public void helper(int[] arr, List<List<Integer>> result, int start) {
+       if(start >= arr.length) {
+           List<Integer> tempList = new ArrayList<>();
+            for(int i = 0;i<arr.length;i++) {
+                tempList.add(arr[i]);
+            }
+           result.add(tempList);
+           return;
+       }
         
-        for(int i=0;i<nums.length;i++) {   
-            if(tempSet.contains(nums[i]))
-                continue;
-
-            tempSet.add(nums[i]);
-            tempList.add(nums[i]);
-            
-            helper(nums, tempList, result, tempSet);
-            
-            tempSet.remove(nums[i]);
-            tempList.remove(tempList.size()-1);
+        for(int i = start;i < arr.length;i++) {
+            swap(arr, start, i);
+            helper(arr,result, start+1);
+            swap(arr,i, start);
         }
+    }
+    
+    public void swap(int [] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
     }
 }
