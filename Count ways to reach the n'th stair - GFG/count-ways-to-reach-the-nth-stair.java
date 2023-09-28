38f387b6d4f4cb59aaac004a0fbf6814f3b91e62
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-09-27 23:07:11 -0700

    Added solution - LeetHub

--- Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.java
+++ Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.java
@@ -36,27 +36,19 @@ class Solution
     //Function to count number of ways to reach the nth stair.
     int countWays(int n)
     {
+        if(n <= 2)
+            return n;
         
         // your code here
-        int[] dp = new int[n + 1];
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
         
-        for(int i=0;i<n;i++) {
-            Arrays.fill(dp, -1);
+        for(int i=3;i<=n;i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % (int) (Math.pow(10,9) + 7);
         }
         
-        backtrack(n, dp);
-        
         return dp[n];
-    }
-    
-    public int backtrack(int n, int[] dp) {
-        if(n <= 2)
-            return dp[n] = n;
-            
-        if(dp[n] != -1)
-            return  dp[n]; 
-        
-        return dp[n] = (backtrack(n-1, dp) + backtrack(n-2, dp)) % (int) (Math.pow(10,9) + 7);
     }
 }
 
