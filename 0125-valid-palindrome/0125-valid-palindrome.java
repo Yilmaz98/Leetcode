25cd804697f2dfd082f81942302ea04ba5c21b61
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-09-30 15:26:03 -0700

    Time: 5 ms (51.67%), Space: 44.3 MB (21.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,29 +1,28 @@
 class Solution {
-    public boolean isAlphaNumeric(String s, int i) {
-         if((s.charAt(i) >='a' && s.charAt(i) <='z') || (s.charAt(i) >='A'  && s.charAt(i) <='Z') || (s.charAt(i) >='0'  && s.charAt(i) <='9')) {
-             return true;
-         }
-        return false;
-    }
     public boolean isPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-        while(i<j) {
-            while(!isAlphaNumeric(s,i) && i<j){
-                i++;
-            } 
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<s.length();i++) {
+            Character fin = Character.toLowerCase(s.charAt(i));
             
-            while(!isAlphaNumeric(s,j) && j>i) {
+            if(fin >= 'a' && fin <= 'z' || fin >='0' && fin <= '9') {
+                sb.append(fin);
+            }
+        }
+        
+        int i = 0;
+        int j = sb.length() - 1;
+        
+        while(i < j) {
+            if(sb.charAt(i) == sb.charAt(j)) {
+                i++;
                 j--;
             }
-            
-            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+            else {
                 return false;
-            
-            i++;
-            j--;
-           
+            }
         }
+        
         return true;
     }
 }
