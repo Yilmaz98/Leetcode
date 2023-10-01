9d1ab95fd02dcc6edbfa43cbf88f66b9e9f9e70c
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-09-30 19:00:15 -0700

    Time: 1 ms (48.02%), Space: 48.7 MB (60.30%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -15,18 +15,18 @@
  */
 class Solution {
     int sum = 0;
-    public void dfs(TreeNode root, int low, int high) {
-        if(root != null) {
-            if(root.val >= low && root.val <= high) {
-                sum += root.val;
-            }
-            dfs(root.left, low, high);
-            dfs(root.right, low, high);
-        }    
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        inorder(root, low, high);
+        return sum;
     }
     
-    public int rangeSumBST(TreeNode root, int low, int high) {
-        dfs(root, low, high);
-        return sum;
+    public void inorder(TreeNode root, int low, int high) {
+        if(root != null) {
+                    inorder(root.left, low, high);
+        if(root.val <= high && root.val >= low) {
+            sum += root.val;
+        }
+        inorder(root.right, low, high);
+        }
     }
 }
