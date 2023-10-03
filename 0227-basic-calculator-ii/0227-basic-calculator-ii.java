a39cd872be303943d96ed3140ae587f0cdb1c2c6
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-10-03 00:11:33 -0700

    Time: 41 ms (11.41%), Space: 46 MB (16.69%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,28 +1,61 @@
 class Solution {
+    public int precedence(char op) {
+        if(op == '/' || op == '*')
+            return 2;
+        else if (op == '+' || op == '-') 
+            return 1;
+         else 
+            return 0;
+    }
+    
     public int calculate(String s) {
-        if (s == null || s.isEmpty()) return 0;
-        int length = s.length();
-        int currentNumber = 0, lastNumber = 0, result = 0;
-        char operation = '+';
-        for (int i = 0; i < length; i++) {
-            char currentChar = s.charAt(i);
-            if (Character.isDigit(currentChar)) {
-                currentNumber = (currentNumber * 10) + (currentChar - '0');
-            }
-            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == length - 1) {
-                if (operation == '+' || operation == '-') {
-                    result += lastNumber;
-                    lastNumber = (operation == '+') ? currentNumber : -currentNumber;
-                } else if (operation == '*') {
-                    lastNumber = lastNumber * currentNumber;
-                } else if (operation == '/') {
-                    lastNumber = lastNumber / currentNumber;
+        Stack<Integer> nums = new Stack<>();
+        Stack<Character> ops = new Stack<>();
+        int ans = 0;
+        
+        for(int i=0;i<s.length();i++) {
+            if(Character.isDigit(s.charAt(i))) {
+                int result = 0;
+                while(i < s.length() && Character.isDigit(s.charAt(i))) {
+                    result = result * 10 + s.charAt(i) - '0';
+                    i++;
                 }
-                operation = currentChar;
-                currentNumber = 0;
+                nums.add(result);
+                i--;
+            } else if(s.charAt(i) == '(') {
+                ops.add(s.charAt(i));
+            } else if(s.charAt(i) == ')') {
+                while(ops.peek() != '(') {
+                    nums.add(calculate(ops.pop(), nums.pop(), nums.pop()));
+                }
+                ops.pop();
+            } else if(s.charAt(i) == '+' || s.charAt(i) == '-' || s.charAt(i) == '*' || s.charAt(i) == '/') {
+            if(s.charAt(i) == '-') {
+                if(nums.isEmpty() || !ops.isEmpty() && ops.peek() == '(') {
+                    nums.add(0);
+                }
+            }
+            while(!ops.isEmpty() && precedence(ops.peek()) >= precedence(s.charAt(i))) {
+                nums.add(calculate(ops.pop(), nums.pop(), nums.pop()));
+            }
+            ops.add(s.charAt(i));
             }
         }
-        result += lastNumber;
-        return result;
+        
+        while(!ops.isEmpty()) {
+            nums.add(calculate(ops.pop(), nums.pop(), nums.pop()));
+        }
+        
+        return nums.pop();
+    }
+    
+    public int calculate(char op, int b, int a) {
+        switch(op) {
+            case '+' : return a + b;
+            case '-' : return a - b;
+            case '*' : return a * b;
+            case '/' : return a / b;
+        }
+        return -1;
     }
 }
