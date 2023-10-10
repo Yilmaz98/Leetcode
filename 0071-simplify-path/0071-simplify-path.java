a1b5330fafdc353387439be36c5cb3adadf49fd8
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-10-09 19:07:50 -0700

    Time: 4 ms (88.67%), Space: 43.6 MB (40.31%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,26 +1,35 @@
 class Solution {
     public String simplifyPath(String path) {
-        Stack<String> st = new Stack<>();
-        
-        String[] paths = path.split("/");
-        
-        for(String p : paths) {
-            if(p.isEmpty() || p.equals(".")) {
+
+        // Initialize a stack
+        Stack<String> stack = new Stack<String>();
+        String[] components = path.split("/");
+
+        // Split the input string on "/" as the delimiter
+        // and process each portion one by one
+        for (String directory : components) {
+
+            // A no-op for a "." or an empty string
+            if (directory.equals(".") || directory.isEmpty()) {
                 continue;
-            }
-            else if(p.equals("..")) {
-                if(!st.isEmpty()) {
-                    st.pop();
+            } else if (directory.equals("..")) {
+
+                // If the current component is a "..", then
+                // we pop an entry from the stack if it's non-empty
+                if (!stack.isEmpty()) {
+                    stack.pop();
                 }
             } else {
-                            st.add(p);
-            }
 
+                // Finally, a legitimate directory name, so we add it
+                // to our stack
+                stack.add(directory);
+            }
         }
-        
-               // Stich together all the directory names together
+
+        // Stich together all the directory names together
         StringBuilder result = new StringBuilder();
-        for (String dir : st) {
+        for (String dir : stack) {
             result.append("/");
             result.append(dir);
         }
