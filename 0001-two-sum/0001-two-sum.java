ac622632102e3162e9736a1c6d597615b64e8913
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-10-20 14:46:06 -0700

    Time: 16 ms (56.23%), Space: 44.7 MB (8.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -7,13 +7,14 @@ class Solution {
             m.put(nums[i], i);
         }
         
-        // Just navigate through the list and see if you have a remaining difference available
+        System.out.println(m);
+        // Just navigate through the list and see if you have a remaining difference available in the map
         for(int i=0;i<nums.length;i++) {
             if(m.containsKey(target - nums[i]) && m.get(target - nums[i]) != i) {
                 return new int[]{i, m.get(target - nums[i])};
             }
         }
         
-        return new int[]{0,0};
+        return new int[]{-1,-1};
     }
 }
