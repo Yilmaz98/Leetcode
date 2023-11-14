b8424a4cea110bebc16c730ea9d01361a091c0a9
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-11-13 22:11:11 -0800

    Time: 40 ms (64.52%), Space: 48.7 MB (5.40%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.java
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.java
@@ -1,19 +1,19 @@
 class MovingAverage {
-  int size, windowSum = 0, count = 0;
-  Deque queue = new ArrayDeque<Integer>();
-
+  int size, head = 0, windowSum = 0, count = 0;
+  int[] queue;
   public MovingAverage(int size) {
     this.size = size;
+    queue = new int[size];
   }
 
   public double next(int val) {
     ++count;
     // calculate the new sum by shifting the window
-    queue.add(val);
-    int tail = count > size ? (int)queue.poll() : 0;
-
-    windowSum = windowSum - tail + val;
-
+    int tail = (head + 1) % size;
+    windowSum = windowSum - queue[tail] + val;
+    // move on to the next head
+    head = (head + 1) % size;
+    queue[head] = val;
     return windowSum * 1.0 / Math.min(size, count);
   }
 }
