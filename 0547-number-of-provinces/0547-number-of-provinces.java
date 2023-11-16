c148918a05e67b923d7fed2febb4007747ab6674
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-11-16 06:51:02 -0800

    Time: 1 ms (94.75%), Space: 46.2 MB (22.41%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,17 +1,5 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        Map<Integer, List<Integer>> m = new HashMap<>();
-        
-        for(int i=0;i<isConnected.length;i++) {
-            for(int j=0;j<isConnected[0].length;j++) {
-                if(i != j && isConnected[i][j] == 1) {
-                    if(!m.containsKey(i))
-                        m.put(i, new ArrayList<>());
-                    m.get(i).add(j);
-                }
-            }
-        }
-        
+    public int findCircleNum(int[][] isConnected) {        
         int n = isConnected.length;
         boolean[] visited = new boolean[n + 1];
         int provinces = 0;
@@ -19,20 +7,19 @@ class Solution {
         for(int i=0;i<n;i++) {
             if(!visited[i]) {
                 provinces++;
-                dfs(m, visited, i);
+                dfs(visited, i, isConnected);
             }
         }
         
         return provinces;
     }
     
-    public void dfs(Map<Integer, List<Integer>> m , boolean[] visited, int i) {
+    public void dfs(boolean[] visited, int i, int[][] isConnected) {
         visited[i] = true;
-        if(!m.containsKey(i))
-            return;
-        for(Integer nei : m.get(i)) {
-            if(!visited[nei]) {
-                dfs(m, visited, nei);
+       
+        for(int j=0;j<isConnected[i].length;j++) {
+            if(i != j && isConnected[i][j] == 1 && !visited[j]) {
+                dfs(visited, j, isConnected);
             }
         }
     }
