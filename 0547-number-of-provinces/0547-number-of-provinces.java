bca39ca667aba88c68612b8d107e8aa5e65cc67a
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-11-24 18:44:50 -0800

    Time: 7 ms (8.61%), Space: 45.4 MB (85.01%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,42 +1,48 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {        
+    public int findCircleNum(int[][] isConnected) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        
+        for(int i=0;i<isConnected.length;i++) {
+            for(int j=0;j<isConnected[i].length;j++) {
+                if(isConnected[i][j] == 1&& i != j) {
+                    if(!adj.containsKey(i))
+                        adj.put(i, new ArrayList<>());
+                    
+                    adj.get(i).add(j);
+                    
+                     if(!adj.containsKey(j))
+                        adj.put(j, new ArrayList<>());
+                    
+                    adj.get(j).add(i);
+                }
+            }
+        }
+        
         int n = isConnected.length;
-        boolean[] visited = new boolean[n + 1];
+        
+        boolean[] visited = new boolean[n];
+        
         int provinces = 0;
         
         for(int i=0;i<n;i++) {
             if(!visited[i]) {
                 provinces++;
-                bfs(visited, i, isConnected);
+                dfs(adj, visited, i);
             }
         }
         
         return provinces;
     }
     
-    public void bfs(boolean[] visited, int i, int[][] isConnected) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(i);
-        visited[i] = true;
+    public void dfs(Map<Integer, List<Integer>> adj, boolean[] visited, int curr) {
+        visited[curr] = true;
         
-        while(!q.isEmpty()) {
-            Integer curr = q.poll();
-            
-            for(int j=0;j<isConnected[curr].length;j++) {
-            if(isConnected[curr][j] == 1 && !visited[j]) {
-                q.add(j);
-                visited[j] = true;
-             }
-            }
-        }
-    }
-    
-    public void dfs(boolean[] visited, int i, int[][] isConnected) {
-        visited[i] = true;
-       
-        for(int j=0;j<isConnected[i].length;j++) {
-            if(isConnected[i][j] == 1 && !visited[j]) {
-                dfs(visited, j, isConnected);
+        if(!adj.containsKey(curr))
+            return;
+        
+        for(Integer nei : adj.get(curr)) {
+            if(!visited[nei]) {
+                dfs(adj, visited, nei);
             }
         }
     }
