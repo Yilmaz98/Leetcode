06b8b62b65a389d2cc8eb85ff7b9c3e6da47bab7
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-11-29 12:04:42 -0800

    Time: 46 ms (5.02%), Space: 48.5 MB (31.21%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,18 +1,32 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        int m = text1.length();
+        int n = text2.length();
         
+        int[][] dp = new int[m][n];
         
-        for(int i=1;i<=text1.length();i++) {
-            for(int j=1;j<=text2.length();j++) {
-                if(text1.charAt(i-1) == text2.charAt(j-1)) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+        for(int i=0;i<m;i++) {
+            Arrays.fill(dp[i], - 1);
+        }
+        return recurse(text1, text2, 0, 0, m, n, dp);
+    }
+    
+    public int recurse(String text1, String text2, int i, int j, int m, int n, int[][] dp) {
+        if(i >= m || j >= n)
+            return 0;
+        
+        int pick = 0;
+        int notPick = 0;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        if(text1.charAt(i) == text2.charAt(j)) {
+            pick = 1 + recurse(text1, text2, i + 1, j + 1, m, n, dp);
         }
         
-        return dp[text1.length()][text2.length()];
+        notPick = Math.max(recurse(text1, text2, i + 1, j, m, n, dp), recurse(text1, text2, i, j+1, m, n, dp));
+        
+        return dp[i][j] = Math.max(pick, notPick);
     }
 }
