afe7f289b1bcd69617e020806704245f9b0bcfa4
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-11-30 09:36:49 -0800

    Time: 0 ms (100.00%), Space: 50 MB (37.31%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,27 +1,11 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int lo = 0;
-        int hi = nums.length - 1;
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            System.out.println(mid);
-            boolean halvesAreEven = (hi - mid) % 2 == 0;
-            if (nums[mid + 1] == nums[mid]) {
-                if (halvesAreEven) {
-                    lo = mid + 2;
-                } else {
-                    hi = mid - 1;
-                }
-            } else if (nums[mid - 1] == nums[mid]) {
-                if (halvesAreEven) {
-                    hi = mid - 2;
-                } else {
-                    lo = mid + 1;
-                }
-            } else {
-                return nums[mid];
-            }
+        int n = 0;
+        
+        for(int i=0;i<nums.length;i++) {
+            n = n ^ nums[i];
         }
-        return nums[lo];
+        
+        return n;
     }
 }
