ca088be4e508018c72d711fb029b1e9bff85ea9d
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-01 10:00:02 -0800

    Time: 9 ms (24.46%), Space: 55 MB (79.95%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,25 +1,36 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), s,0);
+        partition(s, result, new ArrayList<>(), 0);
         return result;
     }
     
-    public void backtrack(List<List<String>> res, List<String> tempList, String s, int start)       {   
-         if (start >= s.length()) res.add(new ArrayList<String>(tempList));
-        for(int i=start;i<s.length();i++) {
-            if(isPalindrome(s,start,i)) {
-                tempList.add(s.substring(start, i+1));
-                backtrack(res,tempList,s,i+1);
-                tempList.remove(tempList.size()-1);
+    public void partition(String s, List<List<String>> result, List<String> temp, int idx) {
+        if(idx == s.length()) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        
+        for(int i=idx;i<s.length();i++) {
+            if(isPalindrome(s.substring(idx,i+1))) {
+                temp.add(s.substring(idx,i+1));
+                partition(s, result, temp, i+1);
+                temp.remove(temp.size() - 1);
             }
         }
-      }
+    }
     
-    public boolean isPalindrome(String s, int low,int high) {
-        while(low<high) {
-            if(s.charAt(low++) != s.charAt(high--)) return false;
+    public boolean isPalindrome(String s) {
+        int low = 0;
+        int high = s.length() -1;
+                
+        while(low <= high) {
+            if(s.charAt(low) != s.charAt(high))
+                return false;
+            low++;
+            high--;
         }
+        
         return true;
     }
 }
