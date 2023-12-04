49205db5b8fdf464f81053fdb8327fb133b687fa
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-04 01:58:17 -0800

    Time: 6 ms (59.70%), Space: 42.9 MB (60.87%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,29 +1,21 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        int[] dp = new int[s.length() + 1];
-        Arrays.fill(dp, -1);
+        int n = s.length();
+        int[] dp = new int[n + 1];  
+        dp[n] = 1;
         
-        return recurse(s, 0, wordDict, dp) == 1? true: false;
-    }
-    
-    public int recurse(String s, int idx, List<String> wordDict, int[] dp) {
-        if(idx >= s.length()) {
-            return dp[idx] = 1;
-        }
-        
-        if(dp[idx] != -1)
-            return dp[idx];
-        
-        for(int i=0;i<wordDict.size();i++) {
-            int end = wordDict.get(i).length();
-            if(idx + end <= s.length()) {
-                if(s.substring(idx, idx + end).equals(wordDict.get(i))) {
-                if(recurse(s, idx + end, wordDict, dp) == 1)
-                    return dp[idx] = 1;
+        for(int i = n;i>=0;i--) {
+            for(int j=0;j<wordDict.size();j++) {
+                int end = wordDict.get(j).length();
+                if(i + end <= n) {
+                    if(s.substring(i, i + end).equals(wordDict.get(j))) {
+                        if(dp[i + end] == 1)
+                            dp[i] = 1;
+                        }
                 }
             }
         }
         
-        return dp[idx] = 0;
+        return dp[0] == 1 ? true : false;
     }
 }
