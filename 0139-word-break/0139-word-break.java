e6150569878a74af750bafe95462b09333f502b2
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-04 01:49:21 -0800

    Time: 2 ms (91.03%), Space: 40.9 MB (87.83%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,28 +1,29 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> words = new HashSet<>(wordDict);
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] seen = new boolean[s.length() + 1];
-        queue.add(0);
+        int[] dp = new int[s.length() + 1];
+        Arrays.fill(dp, -1);
         
-        while (!queue.isEmpty()) {
-            int start = queue.remove();
-            if (start == s.length()) {
-                return true;
-            }
-            
-            for (int end = start + 1; end <= s.length(); end++) {
-                if (seen[end]) {
-                    continue;
-                }
-                
-                if (words.contains(s.substring(start, end))) {
-                    queue.add(end);
-                    seen[end] = true;
+        return recurse(s, 0, wordDict, dp) == 1? true: false;
+    }
+    
+    public int recurse(String s, int idx, List<String> wordDict, int[] dp) {
+        if(idx >= s.length()) {
+            return dp[idx] = 1;
+        }
+        
+        if(dp[idx] != -1)
+            return dp[idx];
+        
+        for(int i=0;i<wordDict.size();i++) {
+            int end = wordDict.get(i).length();
+            if(idx + end <= s.length()) {
+                if(s.substring(idx, idx + end).equals(wordDict.get(i))) {
+                if(recurse(s, idx + end, wordDict, dp) == 1)
+                    return dp[idx] = 1;
                 }
             }
         }
         
-        return false;
+        return dp[idx] = 0;
     }
 }
