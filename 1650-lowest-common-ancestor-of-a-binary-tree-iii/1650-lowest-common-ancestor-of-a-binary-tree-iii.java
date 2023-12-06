5abbe3300cb18be63e62201ca10ccb8312da1dbf
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-06 12:32:40 -0800

    Time: 27 ms (94.65%), Space: 43.6 MB (92.33%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -10,14 +10,21 @@ class Node {
 
 class Solution {
     public Node lowestCommonAncestor(Node p, Node q) {
-          Node runner1 = p, runner2 = q;
+        Node runner1 = p;
+        Node runner2 = q;
         
-        while (runner1 != runner2) { // Guaranteed to complete since both nodes belong to same tree
-            // We are simulating a cycle when any of the conditions below is satisfied 
-            // by pointing runner to the head of the other "list"  to make sure 
-            // intersection is found before either of the below conditions are satisfied again
-            runner1 = (runner1 == null)?q:runner1.parent;
-            runner2 = (runner2 == null)?p:runner2.parent;
+        while(runner1 != runner2) {
+            if(runner1 != null) {
+                runner1 = runner1.parent;
+            } else {
+                runner1 = q;
+            }
+            
+            if(runner2 != null) {
+                runner2 = runner2.parent;
+            } else {
+                runner2 = p;
+            }
         }
         
         return runner1;
