8950b5a3d2637099725f81e6f2699b9b2e713d08
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-06 12:28:28 -0800

    Time: 19 ms (41.13%), Space: 45.5 MB (10.64%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,45 +1,49 @@
 class Solution {
+    int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
     
-    public boolean isValid(int x, int y, int[][] grid, int n) {
-        if(x<0 || x>=n || y<0 || y>=n || grid[x][y] == 1)
-            return false;
-        
-        return true;
+    public boolean isValid(int newX, int newY, int m, int n, int[][] visited, int[][] grid) {
+        if(newX >=0 && newX < m && newY >=0 && newY < n && visited[newX][newY] == 0 && grid[newX][newY] == 0)
+            return true;
+        return false;
     }
+    
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int[][] dirs = {{-1,0}, {0,1}, {0,-1}, {1,0}, {1,-1}, {1,1},{-1,1},{-1,-1}};
-        
+        if(grid.length == 0)
+            return 0;
+            
         if(grid[0][0] == 1)
             return -1;
+        
         Queue<int[]> q = new LinkedList<>();
-        Set<String>visited = new HashSet<>();
         q.add(new int[]{0,0});
         
-        int n = grid.length;
-        visited.add(0 + "," + 0);
+        int m = grid.length;
+        int n = grid[0].length;
         
-        int steps = 1;
+        int[][] visited = new int[m][n];
+        
+        visited[0][0] = 1;
+        int steps = 0;
         
         while(!q.isEmpty()) {
             int size = q.size();
-            for(int i=0;i<size;i++) {
-            int[] curr = q.poll();
-            
-            if(curr[0] == n-1 && curr[1] == n-1) {
-                return steps;
-            }
-            
-            for(int[] dir : dirs) {
-                int newX = curr[0] + dir[0];
-                int newY = curr[1] + dir[1];
-                
-                if(isValid(newX, newY, grid, n) && !visited.contains(newX + "," + newY)) {
-                    visited.add(newX + "," + newY);
-                    q.add(new int[]{newX, newY});
-                }
-            } 
-            }
             steps++;
+            for(int j = 0;j<size;j++) {
+                int[] curr = q.poll();
+                
+                if(curr[0] == m - 1 && curr[1] == n -1)
+                    return steps;
+            
+                for(int i=0;i<8;i++) {
+                    int newDirX = curr[0] + dirs[i][0];
+                    int newDirY = curr[1] + dirs[i][1];
+
+                    if(isValid(newDirX, newDirY, m, n, visited, grid)) {
+                        q.add(new int[]{newDirX, newDirY});
+                        visited[newDirX][newDirY] = 1;
+                    }
+                }
+            }
         }
         
         return -1;
