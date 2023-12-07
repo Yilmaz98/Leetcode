36811882388a4ea888b79ba1a617ecd7ba7de41d
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-06 23:57:44 -0800

    Time: 1451 ms (11.44%), Space: 284.3 MB (7.71%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.java
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.java
@@ -1,36 +1,20 @@
 class Solution {
     public int longestRepeatingSubstring(String s) {
-        int low = 0;
-        int high = s.length() - 1;
-        int ans = 0;
+        int n = s.length();
+        Set<String> seen = new HashSet<>();
+        int maxLen = 0;
         
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            if(checkRepeatingString(s, mid)) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        for(int i=0;i<n;i++) {
+            for(int j = i+1;j<n;j++) {
+                String substr = s.substring(i,j+1);
+                
+                if(seen.contains(substr)) {
+                    maxLen = Math.max(maxLen, substr.length());
+                }
+                seen.add(substr);
             }
         }
         
-        return ans;
+        return maxLen;
     }
-    
-    public boolean checkRepeatingString(String s, int len) {
-        Set<Integer> seen = new HashSet<>();
-        int hashCode = 0;
-        for(int i=0;i<s.length() - len + 1;i++) {
-            String substr = s.substring(i, i + len);
-            hashCode = substr.hashCode();
-            if(seen.contains(hashCode)) {
-                return true;
-            }
-            seen.add(hashCode);
-        }
-        
-        return false;
-    }
-
 }
