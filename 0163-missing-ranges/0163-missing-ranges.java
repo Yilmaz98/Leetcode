e245f854e30c504ca8d7fbffbe11980f0b560765
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-08 11:46:26 -0800

    Time: 0 ms (100.00%), Space: 41 MB (12.66%) - LeetHub

---
+++ 0163-missing-ranges/0163-missing-ranges.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
+        int n = nums.length;
+        
+        if(n == 0) {
+            return Arrays.asList(Arrays.asList(lower, upper));
+        }
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        if(lower < nums[0]) {
+            result.add(Arrays.asList(lower, nums[0] - 1));
+        }
+    
+        
+        for(int i=0;i<n-1;i++) {
+            if(nums[i + 1] - nums[i] <= 1) {
+                continue;
+            }
+            
+            result.add(Arrays.asList(nums[i] +1, nums[i+1] - 1));
+        }
+        
+        if(nums[n-1] < upper) {
+            result.add(Arrays.asList(nums[n-1] + 1, upper));
+        }
+        
+        return result;
+    }
+}
