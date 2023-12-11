cad4b132f81843f300f35066a2caeb908a6c0e7b
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-11 09:35:47 -0800

    Time: 281 ms (16.16%), Space: 54 MB (6.09%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,23 +1,34 @@
 class Solution {
-    int lo, maxLen = 1;
     public String longestPalindrome(String s) {
-        for(int i=0;i<s.length()-1;i++) {
-            extendPalindrome(s,i,i);
-            extendPalindrome(s,i,i+1);
+        int m = s.length();
+        boolean[][] dp = new boolean[m][m];
+        int count = 0;
+        int len = 0;
+        String result = new String();
+        
+        for(int g=0;g<m;g++) {
+            for(int i=0,j=g;j<m;i++,j++) {
+                if(g == 0) {
+                    dp[i][j] = true;
+                } else if(g == 1) {
+                    if(s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = false;
+                    }
+                } else {
+                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1] == true) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = false;
+                    }
+                }
+                if(dp[i][j] == true) {
+                    result = s.substring(i, j + 1);
+                }
+            }
         }
         
-        return s.substring(lo, lo+maxLen);
-    }
-    
-    public void extendPalindrome(String s, int j,  int k) {
-        while(j >=0 && k<s.length() && s.charAt(j) == s.charAt(k)) {
-            j--;
-            k++;
-        }
-        
-        if(maxLen < k-j-1) {
-            maxLen= k-j-1;
-            lo = j+1;
-        }
+        return result;
     }
 }
