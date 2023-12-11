f474351c5da6b5da44c47dff710600fbfb6a15df
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-11 09:26:39 -0800

    Time: 14 ms (26.03%), Space: 43.9 MB (22.16%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,28 +1,29 @@
 class Solution {
     public int countSubstrings(String s) {
-        int n=s.length();
-        boolean[][] dp= new boolean[n][n];
-        int count=0;
+        int m = s.length();
+        boolean[][] dp = new boolean[m][m];
+        int count = 0;
         
-        for(int gap = 0; gap < n; gap++) {
-            int row = 0;
-            for(int col = gap; col < n; col++) {
-                if(gap == 0) {
-                    dp[row][col] = true;
-                }
-                else if(gap == 1) {
-                    if(s.charAt(row) == s.charAt(col)) {
-                        dp[row][col] = true;
+        for(int g=0;g<m;g++) {
+            for(int i=0,j=g;j<m;i++,j++) {
+                if(g == 0) {
+                    dp[i][j] = true;
+                } else if(g == 1) {
+                    if(s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = false;
+                    }
+                } else {
+                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1] == true) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = false;
                     }
                 }
-                else {
-                    if(s.charAt(row) == s.charAt(col) && dp[row+1][col-1] == true) {
-                        dp[row][col] = true;
-                    }
+                if(dp[i][j] == true) {
+                    count++;
                 }
-
-                if(dp[row][col] == true) count++;
-                row++;
             }
         }
         
