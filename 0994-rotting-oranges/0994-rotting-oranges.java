b32609ec8fe9eca15368fcd567792172ea03ea05
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-15 05:22:14 -0800

    Time: 2 ms (84.03%), Space: 40.9 MB (90.00%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,54 +1,59 @@
 class Solution {
-    int[][] dir = {{-1,0},{0,1},{1,0}, {0, -1}};
+    int[][] dirs = {{1,0},{0,1},{-1,0},{0,-1}};
+    
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
         int freshOranges = 0;
+        Queue<int[]> q = new LinkedList<>();
         
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
-                if(grid[i][j] == 2) {
-                    q.add(new int[]{i,j});
-                } else if(grid[i][j] == 1) {
+                if(grid[i][j] == 1) {
                     freshOranges++;
+                } else if(grid[i][j] == 2) {
+                    q.add(new int[]{i,j});
                 }
             }
         }
         
-            if(q.isEmpty()) {
-                if(freshOranges>0)
-                    return -1;
-                else
-                    
+        if(q.isEmpty()) {
+            if(freshOranges > 0)
+                return -1;
+            else 
+                return 0;
+        }
+        
+        if(freshOranges == 0)
             return 0;
-            }
         
         int time = 0;
+        int minutes = 0;
+        
         while(!q.isEmpty()) {
-            int size = q.size();            
+            int size = q.size();    
+            minutes++;
             for(int i=0;i<size;i++) {
                 int[] curr = q.poll();
                 
-                for(int[] nei : dir) {
-                    int newX = curr[0] + nei[0];
-                    int newY = curr[1] + nei[1];
+                for(int[] dir : dirs) {
+                    int newX = curr[0] + dir[0];
+                    int newY = curr[1] + dir[1];
                     
-                    if(newX >=0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] == 1) {
+                    if(newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] == 1) {
                         grid[newX][newY] = 2;
-                        freshOranges--;
                         q.add(new int[]{newX, newY});
+                        freshOranges--;
                     }
                 }
             }
-            time++;
+            if(freshOranges == 0) 
+                return minutes;
         }
         
-
-        
-        if(freshOranges > 0)
+        if(freshOranges != 0)
             return -1;
         
-        return time - 1;
+        return minutes;
     }
 }
