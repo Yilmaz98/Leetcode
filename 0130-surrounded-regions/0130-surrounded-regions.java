680a663e2f4bdf6d617c6718fc76374a257a9009
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-15 12:08:11 -0800

    Time: 4 ms (22.46%), Space: 44.4 MB (24.94%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,17 +1,37 @@
 class Solution {
+    int[][] dirs = {{1,0}, {-1,0}, {0, 1}, {0,-1}};
     public void solve(char[][] board) {
-        // boundary start dfs
         int m = board.length;
         int n = board[0].length;
         
-        boolean[][] visited = new boolean[m][n];
+        int[][] visited = new int[m][n];
+        Queue<int[]> q = new LinkedList<>();
         
         for(int i=0;i<m;i++) {
-            for(int j = 0;j<n;j++) {
-                if(i ==0 || i == m - 1 || j == 0 || j == n-1) {
+            for(int j=0;j<n;j++) {
+                if(i == 0 || j == 0 || i == m - 1 || j == n - 1) {
                     if(board[i][j] == 'O') {
-                        if(!visited[i][j])
-                            dfs(board,i,j,visited,m,n);
+                        q.add(new int[]{i,j});
+                        visited[i][j] = 1;
+                    }
+                }
+            }
+        }
+        
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i=0;i<size;i++) {
+                int[] curr = q.poll();
+                
+                for(int[] dir : dirs) {
+                    int newX = curr[0] + dir[0];
+                    int newY = curr[1] + dir[1];
+                    
+                    if(newX >=0 && newX < m && newY >= 0 && newY < n && board[newX][newY] == 'O' && visited[newX][newY] == 0) {
+                        visited[newX][newY] = 1;
+                        q.add(new int[]{newX, newY});
                     }
                 }
             }
@@ -19,24 +39,10 @@ class Solution {
         
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
-                if(board[i][j] == 'O') {
-                    if(!visited[i][j]) {
-                        board[i][j] = 'X';
-                    }
+                if(visited[i][j] == 0 && board[i][j] == 'O') {
+                    board[i][j] = 'X';
                 }
             }
         }
-    }
-    
-    
-    public void dfs(char[][] board, int i, int j, boolean[][] visited, int m, int n) {
-        if(i < 0 || i >= m || j < 0 || j >=n || board[i][j] == 'X' || visited[i][j])
-            return;
-        
-        visited[i][j] = true;
-        dfs(board,i+1,j,visited,m,n);
-        dfs(board,i,j+1,visited,m,n);
-        dfs(board,i-1,j,visited,m,n);
-        dfs(board,i,j-1,visited,m,n);
     }
 }
