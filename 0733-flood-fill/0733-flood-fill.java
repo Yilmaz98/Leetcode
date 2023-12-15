57a3b03c3608143afa72cb308af9f2222c968bee
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-15 04:55:23 -0800

    Time: 0 ms (100.00%), Space: 43.9 MB (64.88%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,18 +1,21 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        floodfill(image, sr, sc, color, image[sr][sc]);
+        if(image[sr][sc] == color)
+            return image;
+        
+        yiluFill(image, sr, sc, color, image[sr][sc]);
+        
         return image;
     }
     
-    public void floodfill(int[][] image, int sr, int sc, int color, int currColor) {
-        if(sr <0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != currColor || color == currColor)
+    public void yiluFill(int[][] image, int sr, int sc, int color, int oldColor) {
+        if(sr < 0 || sr >= image.length || sc <0 || sc >= image[0].length || image[sr][sc] != oldColor)
             return;
         
         image[sr][sc] = color;
-        
-        floodfill(image, sr + 1, sc, color, currColor);
-        floodfill(image, sr, sc + 1, color, currColor);
-        floodfill(image, sr - 1, sc, color, currColor);
-        floodfill(image, sr, sc - 1, color, currColor);
+        yiluFill(image, sr - 1, sc, color, oldColor);
+        yiluFill(image, sr , sc - 1, color, oldColor);
+        yiluFill(image, sr + 1, sc, color, oldColor);
+        yiluFill(image, sr, sc + 1, color, oldColor);
     }
 }
