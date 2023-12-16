ebbcb649d9654849262cbc069596b28e1652f667
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-16 09:20:33 -0800

    Time: 13 ms (13.35%), Space: 44 MB (68.60%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,45 +1,39 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int[] indegree = new int[numCourses];
-        
-        for(int[] prereq : prerequisites) {
-            indegree[prereq[0]]++;
-        }
-        
         Map<Integer, List<Integer>> adj = new HashMap<>();
         
-        for(int[] curr : prerequisites) {
+        for(int[] curr: prerequisites) {
             if(!adj.containsKey(curr[1])) {
                 adj.put(curr[1], new ArrayList<>());
             }
             
             adj.get(curr[1]).add(curr[0]);
-         }
+            indegree[curr[0]]++;
+        }
+        
+        System.out.println(adj);
+        
         
         Queue<Integer> q = new LinkedList<>();
         
-        for(int i=0;i<indegree.length;i++) {
-            if(indegree[i] == 0) {
-                 q.add(i);
-            }
+        for(int i=0;i<numCourses;i++) {
+            if(indegree[i] == 0)
+                q.add(i);
         }
         
-       
-        int index = 0;
-        
+        int sizes = 0;
         while(!q.isEmpty()) {
             Integer curr = q.poll();
-            index++;
+            sizes++;
             
-            if(!adj.containsKey(curr))
-                continue;
-            
-            for(Integer nei : adj.get(curr)) {
-                if(--indegree[nei] == 0)
+            for(Integer nei : adj.getOrDefault(curr, new ArrayList<>())) {
+                if(--indegree[nei] == 0) {
                     q.add(nei);
+                }
             }
         }
         
-        return index == numCourses;
+        return sizes == numCourses;
     }
 }
