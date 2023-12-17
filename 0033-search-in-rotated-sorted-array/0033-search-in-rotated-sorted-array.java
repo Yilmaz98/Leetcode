2c1a94b20b8bb6c8bbbb29bde14d31d232d3de98
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-16 16:57:01 -0800

    Time: 0 ms (100.00%), Space: 41.4 MB (20.01%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,29 +1,28 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int result = 0;
         int n = nums.length;
         
-        if(nums[0] == target) return 0;
+        int low = 0;
+        int high = n - 1;
+        int ans = -1;
         
-        if(nums[n - 1] == target) return n - 1;
-        
-        int low = 1;
-        int high = n - 2;
-           
         while(low <= high) {
             int mid = low + (high - low) / 2;
             
-            if(nums[mid] == target)
-                return mid;
-            
             if(nums[low] <= nums[mid]) {
                 if(nums[low] <= target && target <= nums[mid]) {
+                    if(nums[mid] == target) {
+                        ans = mid;
+                    }
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
             } else {
                 if(nums[mid] <= target && target <= nums[high]) {
+                    if(nums[mid] == target) {
+                        ans = mid;
+                    }
                     low = mid + 1;
                 } else {
                     high = mid - 1;
@@ -31,6 +30,6 @@ class Solution {
             }
         }
         
-        return -1;
+        return ans;
     }
 }
