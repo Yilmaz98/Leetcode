781bcc356c6d7fee829e6229d184957e24cccd89
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-19 22:24:26 -0800

    Time: 359 ms (8.69%), Space: 110.7 MB (5.10%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,21 +1,34 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
+        int n = nums.length;
+        int[][] dp = new int[n][n + 1];
         
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
+        for(int i=0;i<n;i++) {
+           Arrays.fill(dp[i], -1); 
         }
         
-        int longest = 0;
-        for (int c: dp) {
-            longest = Math.max(longest, c);
+        return recurse(nums, 0, -1, dp);
+    }
+    
+    public int recurse(int[] nums, int start, int prev, int[][] dp) {
+        if(start == nums.length - 1) {
+            if(prev == -1 || nums[start] > nums[prev]) {
+                return 1;
+            } else
+                return 0;
         }
         
-        return longest;
+        if(dp[start][prev + 1] != -1)
+            return dp[start][prev + 1];
+                
+        int pick = 0;
+        int notPick = 0;
+        
+        if(prev == -1 || nums[start] > nums[prev])
+            pick = 1 + recurse(nums, start + 1, start, dp);
+        
+        notPick = recurse(nums, start + 1, prev, dp);
+        
+        return dp[start][prev + 1] = Math.max(pick, notPick);
     }
 }
