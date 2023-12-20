5d3ee67d3da1aad8f2c7a77922695bcd77196706
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-19 19:59:31 -0800

    Time: 66 ms (27.17%), Space: 74.8 MB (21.74%) - LeetHub

---
+++ 1858-longest-word-with-all-prefixes/1858-longest-word-with-all-prefixes.java
@@ -0,0 +1,77 @@
+class Solution {
+    class TrieNode {
+        Map<Character, TrieNode> children;
+        boolean isWord;
+        boolean isPrefixPresent;
+        
+        TrieNode() {
+            children = new HashMap<>();
+            isWord = false;
+            isPrefixPresent = false;
+        }
+        
+    }
+    
+    class Trie {
+        TrieNode root;
+        
+        Trie() {
+            root = new TrieNode();
+        }
+        
+        public void insert(String word) {
+            TrieNode ws = root;
+            
+            for(Character c : word.toCharArray()) {
+                if(!ws.children.containsKey(c)) {
+                    ws.children.put(c, new TrieNode());
+                }
+                ws = ws.children.get(c);
+            }
+            
+            ws.isWord = true;
+            ws.isPrefixPresent = true;
+        }
+        
+        public boolean isAllPrefixPresent(String word) {
+            TrieNode ws = root;
+            
+            for(Character c : word.toCharArray()) {
+                if(!ws.children.containsKey(c)) {
+                    return false;
+                }
+                ws = ws.children.get(c);
+                if(!ws.isPrefixPresent)
+                    return false;
+            }
+            
+            return true;
+        }
+    }
+    
+    public String longestWord(String[] words) {
+        Trie trie = new Trie();
+        
+        for(String word : words) {
+            trie.insert(word);    
+        }
+        
+        String result = "";
+        
+        int longest = 0;
+        for(String word : words) {
+            if(trie.isAllPrefixPresent(word)) {
+                if(longest < word.length()) {
+                    longest = word.length();
+                    result = word;
+                } else if(longest == word.length()) {
+                    if(word.compareTo(result) < 0)  {
+                        result = word;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+}
