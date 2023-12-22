f1e72662dbe0f216c86bbd7a36540fbd124ad40d
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-22 10:41:19 -0800

    Time: 0 ms (100.00%), Space: 40.1 MB (6.36%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,7 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
+        
         for(int i=0;i<m;i++) {
             Arrays.fill(dp[i], -1);
         }
@@ -9,13 +10,8 @@ class Solution {
     }
     
     public int recurse(int m, int n, int i, int j, int[][] dp) {
-        if(i == m - 1 && j == n - 1){
+        if(i == m - 1 && j == n - 1)
             return 1;
-        }
-        
-        if(i >= m || j >= n) {
-            return 0;
-        }
         
         if(dp[i][j] != -1)
             return dp[i][j];
@@ -24,10 +20,11 @@ class Solution {
         int down = 0;
         
         if(j + 1 < n)
-            right = recurse(m,n,i,j+1,dp);
+            right = recurse(m, n, i, j + 1, dp);
         
         if(i + 1 < m)
-            down = recurse(m,n,i+1,j,dp);
+            down = recurse(m, n, i + 1, j, dp);
+        
         
         return dp[i][j] = down + right;
     }
