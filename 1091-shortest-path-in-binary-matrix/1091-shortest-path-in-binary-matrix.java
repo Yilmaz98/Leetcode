3b17aefa2dd64e15868cb3424255f6da3ad7253b
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-21 21:20:56 -0800

    Time: 141 ms (5.02%), Space: 47.1 MB (5.40%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,49 +1,49 @@
 class Solution {
-    int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
+    int[][] dirs = {{-1,0},{1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
     
-    public boolean isValid(int newX, int newY, int m, int n, int[][] visited, int[][] grid) {
-        if(newX >=0 && newX < m && newY >=0 && newY < n && visited[newX][newY] == 0 && grid[newX][newY] == 0)
+    public boolean isValid(int newX, int newY, int m, int n, Set<String> visited, int[][] grid) {
+        if(newX >=0 && newX < m && newY >=0 && newY < n && !visited.contains(newX + ":" + newY) && grid[newX][newY] == 0) {
             return true;
+        }
+        
         return false;
     }
     
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if(grid.length == 0)
-            return 0;
-            
+        int m = grid.length;
+        int n = grid[0].length;
+        
         if(grid[0][0] == 1)
             return -1;
         
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{0,0});
         
-        int m = grid.length;
-        int n = grid[0].length;
-        
-        int[][] visited = new int[m][n];
-        
-        visited[0][0] = 1;
+        Set<String> visited = new HashSet<>();
         int steps = 0;
         
+        visited.add(0 + ":" + 0);
+        
         while(!q.isEmpty()) {
-            int size = q.size();
-            steps++;
-            for(int j = 0;j<size;j++) {
+            int size = q.size();     
+            steps++;  
+            
+            for(int k = 0;k<size;k++) {
                 int[] curr = q.poll();
                 
-                if(curr[0] == m - 1 && curr[1] == n -1)
-                    return steps;
+                if(curr[0] == m -1 && curr[1] == n -1)
+                        return steps;
             
                 for(int i=0;i<8;i++) {
-                    int newDirX = curr[0] + dirs[i][0];
-                    int newDirY = curr[1] + dirs[i][1];
+                    int newX = curr[0] + dirs[i][0];
+                    int newY = curr[1]  + dirs[i][1];
 
-                    if(isValid(newDirX, newDirY, m, n, visited, grid)) {
-                        q.add(new int[]{newDirX, newDirY});
-                        visited[newDirX][newDirY] = 1;
+                    if(isValid(newX, newY, m, n, visited, grid)) {
+                        q.add(new int[]{newX, newY});
+                        visited.add(newX + ":"  + newY);
                     }
-                }
-            }
+                }   
+            }  
         }
         
         return -1;
