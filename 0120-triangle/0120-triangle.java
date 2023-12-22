50f2709b149fb4e84fe87a5f4cf0a22f8946d43c
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-22 15:43:33 -0800

    Time: 6 ms (27.50%), Space: 44 MB (57.79%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -2,29 +2,29 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int m = triangle.size();
         int[][] dp = new int[m][m];
-        for(int i=0;i<m;i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        
-        return recurse(triangle, 0, 0, m, dp);
-    }
-    
-    public int recurse(List<List<Integer>> triangle, int i, int j, int m, int[][] dp) {
-        if(i == m -1) {
-            return triangle.get(i).get(j);
-        }
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
         
         int down = Integer.MAX_VALUE;
         int diagonal = Integer.MAX_VALUE;
         
-        if(i + 1 < m)
-            down = triangle.get(i).get(j) +  recurse(triangle, i + 1, j, m, dp);
-        if(i + 1 < m && j + 1 < triangle.get(i + 1).size())
-            diagonal = triangle.get(i).get(j) +  recurse(triangle, i + 1, j + 1, m, dp);
+        for(int i = m-1;i>=0;i--) {
+            for(int j = triangle.get(i).size() - 1;j>=0;j--) {
+                if(i == m- 1) {
+                    dp[i][j] = triangle.get(i).get(j);
+                } else {
+                    down = Integer.MAX_VALUE;
+                    diagonal = Integer.MAX_VALUE;
+                    
+                    if(i + 1 < m) 
+                        down = triangle.get(i).get(j) + dp[i+1][j];
+                    
+                    if(i + 1 < m && j + 1 < triangle.get(i+1).size()) 
+                        diagonal = triangle.get(i).get(j) + dp[i+1][j+1];
+                    
+                    dp[i][j] = Math.min(down, diagonal);
+                }
+            }
+        }
         
-        return dp[i][j] = Math.min(down, diagonal);
+        return dp[0][0];
     }
 }
