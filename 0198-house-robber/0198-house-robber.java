24befc4240ad1e719d03959d30383673e221fe95
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-22 09:46:46 -0800

    Time: 0 ms (100.00%), Space: 41.1 MB (6.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,9 +3,19 @@ class Solution {
         int n = nums.length;     
         
         int[] dp = new int[n];
-        Arrays.fill(dp, -1);
+        dp[n - 1] = nums[n-1];
         
-        return recurse(nums, 0, n, dp);
+        int pick = 0;
+        int notPick = 0;
+        
+        for(int i = n - 2;i>=0;i--) {
+            pick = nums[i] + (i + 2 >= n ? 0 : dp[i + 2]);
+            notPick =  (i + 1 >= n ? 0 : dp[i + 1]);
+            
+            dp[i] = Math.max(pick, notPick);
+        }
+        
+        return dp[0];
     }
     
     public int recurse(int[] nums, int start, int n, int[] dp) {
