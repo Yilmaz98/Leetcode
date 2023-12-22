de906d276029ee178346d8bb3f28807980878d4e
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-22 12:53:09 -0800

    Time: 5 ms (7.99%), Space: 45.8 MB (5.14%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -5,29 +5,26 @@ class Solution {
         
         int[][] dp = new int[m][n];
         
-        for(int i=0;i<m;i++) {
-            Arrays.fill(dp[i], -1);
+        for(int i = m-1;i>=0;i--) {
+            for(int j = n -1;j>=0;j--) {
+                if(i == m - 1 && j == n-1) {
+                    dp[i][j] = grid[i][j];
+                } else {
+                    int down = Integer.MAX_VALUE;
+                    int right = Integer.MAX_VALUE;
+                    
+                    if(j + 1 < n) {
+                        right = grid[i][j] + dp[i][j+1];
+                    }
+                    
+                    if(i + 1 < m)
+                        down = grid[i][j] + dp[i+1][j];
+                    
+                    dp[i][j] = Math.min(down, right);
+                }
+            }
         }
         
-        return recurse(grid, m, n, 0, 0, dp);
-    }
-    
-    public int recurse(int[][] grid, int m, int n, int i, int j, int[][] dp) {
-        if(i == m - 1 && j == n - 1)
-            return grid[i][j];
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int right = Integer.MAX_VALUE;
-        int down = Integer.MAX_VALUE;
-        
-        if(j + 1 < n)
-            right = grid[i][j] + recurse(grid, m, n, i , j + 1, dp);
-        
-        if(i + 1 < m)
-            down = grid[i][j] + recurse(grid, m, n, i + 1, j, dp);
-        
-        return dp[i][j] = Math.min(down, right);
+        return dp[0][0];
     }
 }
