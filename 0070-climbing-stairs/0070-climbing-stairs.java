11def21bd7e644951e5827904072914548b0851d
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-22 01:17:42 -0800

    Time: 0 ms (100.00%), Space: 40.3 MB (6.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,27 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
+        int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
+        
         return recurse(n, dp);
     }
     
     public int recurse(int n, int[] dp) {
         if(n == 0)
-            return dp[n] = 0;
-        if(n == 1)
-            return dp[n] = 1;
-        if(n == 2)
-            return dp[n] = 2;
+            return 1;
+        
+        if(n < 0)
+            return 0;
         
         if(dp[n] != -1)
             return dp[n];
         
-        return dp[n] = recurse(n-1, dp) + recurse(n-2, dp);
+        int firstStep = 0;
+        int secondStep = 0;
+        
+        firstStep = recurse(n - 1, dp);
+        secondStep = recurse(n - 2, dp);
+        
+        return dp[n] = firstStep + secondStep;
     }
 }
