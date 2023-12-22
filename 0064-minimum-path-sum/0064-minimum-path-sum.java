95a65ace513584581420b1305bcae715c663b930
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-22 12:50:14 -0800

    Time: 1 ms (96.80%), Space: 45.2 MB (5.14%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -2,27 +2,32 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int result = Integer.MAX_VALUE;
-        int[][] memo = new int[m][n];
-        helper(0,0,grid,memo);
         
-        return memo[0][0];
-    }
-    
-    
-    public int helper(int x, int y, int[][] grid, int[][] memo) {
-        if(x == grid.length -1 && y == grid[0].length-1)
-            return memo[x][y] = grid[x][y];
+        int[][] dp = new int[m][n];
         
-        if(x >= grid.length || y >= grid[0].length)
-            return Integer.MAX_VALUE;
-        
-        if(memo[x][y] != 0) {
-            return memo[x][y];
+        for(int i=0;i<m;i++) {
+            Arrays.fill(dp[i], -1);
         }
         
-        int result = Math.min(helper(x+1,y,grid,memo), helper(x,y+1,grid,memo)) + grid[x][y];
+        return recurse(grid, m, n, 0, 0, dp);
+    }
+    
+    public int recurse(int[][] grid, int m, int n, int i, int j, int[][] dp) {
+        if(i == m - 1 && j == n - 1)
+            return grid[i][j];
         
-        return memo[x][y] = result;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        int right = Integer.MAX_VALUE;
+        int down = Integer.MAX_VALUE;
+        
+        if(j + 1 < n)
+            right = grid[i][j] + recurse(grid, m, n, i , j + 1, dp);
+        
+        if(i + 1 < m)
+            down = grid[i][j] + recurse(grid, m, n, i + 1, j, dp);
+        
+        return dp[i][j] = Math.min(down, right);
     }
 }
