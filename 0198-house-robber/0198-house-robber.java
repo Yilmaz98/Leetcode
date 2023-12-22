ecc0976ba5fb93ce308ab629d152d7064297a815
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-22 09:49:07 -0800

    Time: 0 ms (100.00%), Space: 40.8 MB (6.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,39 +2,22 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;     
         
-        int[] dp = new int[n];
-        dp[n - 1] = nums[n-1];
-        
         int pick = 0;
         int notPick = 0;
+        
+        int nextNext = nums[n-1];
+        int next = nums[n-1];
+        int curr = nums[n-1];
         
         for(int i = n - 2;i>=0;i--) {
-            pick = nums[i] + (i + 2 >= n ? 0 : dp[i + 2]);
-            notPick =  (i + 1 >= n ? 0 : dp[i + 1]);
+            pick = nums[i] + (i + 2 >= n ? 0 : nextNext);
+            notPick =  (i + 1 >= n ? 0 : next);
             
-            dp[i] = Math.max(pick, notPick);
+            curr = Math.max(pick, notPick);
+            nextNext = next;
+            next = curr;
         }
         
-        return dp[0];
-    }
-    
-    public int recurse(int[] nums, int start, int n, int[] dp) {
-        if(start == n - 1) {
-            return nums[start];
-        }
-        
-        if(start >= n)
-            return 0;
-        
-        if(dp[start] != -1)
-            return dp[start];
-        
-        int pick = 0;
-        int notPick = 0;
-        
-        pick = nums[start] + recurse(nums, start + 2, n, dp);
-        notPick = recurse(nums, start + 1, n, dp);
-        
-        return dp[start] = Math.max(pick, notPick);
+        return curr;
     }
 }
