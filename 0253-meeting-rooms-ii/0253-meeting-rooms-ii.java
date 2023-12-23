1b3c575988cdb11d87e0f2c994fb1bb44b6aef79
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-23 10:06:41 -0800

    Time: 4 ms (89.52%), Space: 46.1 MB (5.10%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,21 +1,37 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        int result = 1;
+        int n = intervals.length;
         
-        for(int[] interval : intervals) {
-            if(!pq.isEmpty()) {
-                if(pq.peek()[1] <= interval[0]) {
-                    pq.poll();
-                } else {
-                    result++;
-                }
-            }
-            pq.add(new int[]{interval[0], interval[1]});
+        if (n == 0) {
+            return 0;
         }
         
-        return result;
+        int[] start = new int[n];
+        int[] end = new int[n];
         
+        for(int i=0;i<intervals.length;i++) {
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+        
+        Arrays.sort(start);
+        Arrays.sort(end);
+        
+        
+        int minConferenceRooms = 0;
+        
+        int p1 = 0;
+        int p2 = 0;
+        
+        while(p1 < n) {
+            if(start[p1] >= end[p2]) {
+                minConferenceRooms--;
+                p2++;
+            }
+            minConferenceRooms++;
+            p1++;
+        }
+        
+        return minConferenceRooms;
     }
 }
