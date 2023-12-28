d764809ecadeef88de12640d2795d9dc152a812c
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-28 10:13:13 -0800

    Time: 16 ms (58.41%), Space: 45.1 MB (5.48%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,22 +1,30 @@
 class Solution {
+    int total = 0;
     public int findTargetSumWays(int[] nums, int target) {
-        return recurse(nums, target, 0);
+        total = Arrays.stream(nums).sum();
+        int[][] dp = new int[nums.length][2 * total + 1];
+        for(int i=0;i<nums.length;i++)
+            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        return recurse(nums, target, 0, dp, 0);
     }
     
-    public int recurse(int[] nums, int target, int idx) {
+    public int recurse(int[] nums, int target, int idx, int[][] dp, int sum) {
         if(idx == nums.length) {
-            if(target == 0)
+            if(target == sum)
                 return 1;
             
             return 0;
         }
+                
+        if(dp[idx][sum + total] != Integer.MIN_VALUE)
+            return dp[idx][sum + total];
         
         int pos = 0;
         int neg = 0;
         
-        pos = recurse(nums, target - nums[idx], idx+1);
-        neg = recurse(nums, target + nums[idx], idx+1);
+        pos = recurse(nums, target, idx+1, dp, sum + nums[idx]);
+        neg = recurse(nums, target, idx+1, dp, sum - nums[idx]);
         
-        return pos + neg;
+        return dp[idx][sum + total] = pos + neg;
     }
 }
