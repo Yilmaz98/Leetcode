5571062ea4cea35e4077d7c771845d4b902d540e
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2023-12-28 09:59:11 -0800

    Time: 1018 ms (5.03%), Space: 41 MB (61.03%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -3,30 +3,20 @@ class Solution {
         return recurse(nums, target, 0);
     }
     
-    public int recurse(int[] nums, int target, int start) {
-        if(start == nums.length - 1) {
-            int temp = 0 + nums[start];
-            int temp2 = 0 - nums[start];
-            
-            if(target - temp == 0 && target - temp2 == 0)
-                return 2;
-            
-            if(target - temp == 0 || target - temp2 == 0)
+    public int recurse(int[] nums, int target, int idx) {
+        if(idx == nums.length) {
+            if(target == 0)
                 return 1;
             
             return 0;
         }
         
+        int pos = 0;
+        int neg = 0;
         
-        int plus = 0;
-        int minus = 0;
+        pos = recurse(nums, target - nums[idx], idx+1);
+        neg = recurse(nums, target + nums[idx], idx+1);
         
-        int temp = 0 + nums[start];
-        plus = recurse(nums, target - temp, start + 1);
-            
-        int temp2 = 0 - nums[start];
-        minus = recurse(nums, target - temp2, start + 1);
-        
-        return plus + minus;
+        return pos + neg;
     }
 }
