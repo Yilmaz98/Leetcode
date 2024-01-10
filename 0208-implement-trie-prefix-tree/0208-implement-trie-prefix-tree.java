4c184fe8228344fa3aae26721e316020a4a80eda
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2024-01-09 18:25:17 -0800

    Time: 43 ms (36.84%), Space: 55 MB (54.48%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,55 +1,53 @@
 class TrieNode {
-    Map<Character, TrieNode> children;
+    Map<Character, TrieNode> childrenMap;
     boolean isWord;
-    
+        
     TrieNode() {
-        children = new HashMap<>();
+        childrenMap = new HashMap<>();
         isWord = false;
     }
 }
 
 class Trie {
     TrieNode root;
+    
     public Trie() {
         root = new TrieNode();
     }
     
     public void insert(String word) {
         TrieNode ws = root;
-
-        for(Character c : word.toCharArray()) {
-            if(!ws.children.containsKey(c)) {
-                ws.children.put(c, new TrieNode());
-            }
-            ws = ws.children.get(c);
-        }
         
+        for(Character c : word.toCharArray()) {
+            if(!ws.childrenMap.containsKey(c)) {
+                ws.childrenMap.put(c, new TrieNode());
+            }
+            ws = ws.childrenMap.get(c);
+        }
         ws.isWord = true;
     }
     
     public boolean search(String word) {
-         TrieNode ws = root;
+        TrieNode ws = root;
         
         for(Character c : word.toCharArray()) {
-            if(!ws.children.containsKey(c)) {
+            if(!ws.childrenMap.containsKey(c)) {
                 return false;
             }
-            ws = ws.children.get(c);
+            ws = ws.childrenMap.get(c);
         }
-        
         return ws.isWord;
     }
     
     public boolean startsWith(String prefix) {
-          TrieNode ws = root;
+        TrieNode ws = root;
         
         for(Character c : prefix.toCharArray()) {
-            if(!ws.children.containsKey(c)) {
+            if(!ws.childrenMap.containsKey(c)) {
                 return false;
             }
-            ws = ws.children.get(c);
+             ws = ws.childrenMap.get(c);
         }
-        
         return true;
     }
 }
