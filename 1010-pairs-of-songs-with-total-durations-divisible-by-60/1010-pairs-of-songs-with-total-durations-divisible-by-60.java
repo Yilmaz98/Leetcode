6e49f6db89995ceeb7b1ec33be3d75f953ecad7c
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2024-01-17 23:31:09 -0800

    Time: 2 ms (95.52%), Space: 50.9 MB (73.77%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,15 +1,12 @@
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
-        int remainders[] = new int[60];
-        int count = 0;
-        for (int t: time) {
-            if (t % 60 == 0) { // check if a%60==0 && b%60==0
-                count += remainders[0];
-            } else { // check if a%60+b%60==60
-                count += remainders[60 - t % 60];
-            }
-            remainders[t % 60]++; // remember to update the remainders
+        int[] arr = new int[60];
+        int ans = 0;
+        for(int t : time) {
+            int r = t % 60;
+            ans = ans + arr[(60 - r) % 60];
+            arr[r]++;
         }
-        return count;
+        return ans;
     }
 }
