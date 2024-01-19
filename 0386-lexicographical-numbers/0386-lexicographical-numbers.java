5f06203ef3b674bd78f225d995ce930e80a68bd4
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2024-01-18 18:21:26 -0800

    Time: 16 ms (17.15%), Space: 47.4 MB (78.06%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,20 +1,27 @@
-public class Solution {
+class Solution {
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> res = new ArrayList<>();
-        for(int i=1;i<10;++i){
-          dfs(i, n, res); 
-        }
-        return res;
+        List<Integer> result = new ArrayList<>();
+        
+        backtrack(n, result, new StringBuilder(), true);
+        
+        return result;
     }
     
-    public void dfs(int cur, int n, List<Integer> res){
-        if(cur>n)
-            return;
-        else{
-            res.add(cur);
-            for(int i=0;i<10;++i){
-                dfs(10*cur+i, n, res);
+    public void backtrack(int n, List<Integer> result, StringBuilder sb, boolean firstTime) { 
+        int start = firstTime ? 1 : 0;
+        for(int i = start;i<=9;i++) {
+            sb.append(i);
+            int val = Integer.parseInt(sb.toString());
+        
+            if(val > n) {
+                sb.deleteCharAt(sb.length() -1);
+                return;
+            }   
+            else {    
+                result.add(val);
+                backtrack(n, result, sb, false);
             }
+            sb.deleteCharAt(sb.length() -1);
         }
     }
 }
