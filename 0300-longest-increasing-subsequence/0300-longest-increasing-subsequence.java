db72fac5338ad10e9728e1e250df5167f15f299b
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2024-01-26 12:08:45 -0800

    Time: 52 ms (21.58%), Space: 44.7 MB (17.10%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,34 +1,41 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n][n + 1];
-        
-        for(int i=0;i<n;i++) {
-           Arrays.fill(dp[i], -1); 
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int[] hash = new int[n];
+
+        for(int i = 0;i<n;i++) {
+            hash[i] = i;
+        }
+
+        int lastIndex = 0;
+        int maxi = 1;
+
+        for(int i = 1;i<n;i++) {
+            for(int prev = 0;prev<i;prev++) {
+                if(nums[i] > nums[prev] && dp[i] < (1 + dp[prev])) {
+                    dp[i] = 1 + dp[prev];
+                    hash[i] = prev;
+                    if(maxi < dp[i]) {
+                        maxi = dp[i];
+                        lastIndex = i;
+                    }
+                }
+            }
+        }
+
+        List<Integer> arr = new ArrayList<>();
+
+        while(hash[lastIndex] != lastIndex) {
+            arr.add(nums[lastIndex]);
+            lastIndex = hash[lastIndex];
         }
         
-        return recurse(nums, 0, -1, dp);
-    }
-    
-    public int recurse(int[] nums, int start, int prev, int[][] dp) {
-        if(start == nums.length - 1) {
-            if(prev == -1 || nums[start] > nums[prev]) {
-                return 1;
-            } else
-                return 0;
-        }
-        
-        if(dp[start][prev + 1] != -1)
-            return dp[start][prev + 1];
-                
-        int pick = 0;
-        int notPick = 0;
-        
-        if(prev == -1 || nums[start] > nums[prev])
-            pick = 1 + recurse(nums, start + 1, start, dp);
-        
-        notPick = recurse(nums, start + 1, prev, dp);
-        
-        return dp[start][prev + 1] = Math.max(pick, notPick);
+        arr.add(nums[lastIndex]);
+
+        Collections.reverse(arr);
+
+        return maxi;
     }
 }
