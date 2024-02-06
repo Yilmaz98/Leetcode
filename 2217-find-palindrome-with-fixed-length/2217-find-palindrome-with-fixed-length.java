fa1d9b539fc92e77c6fb8dcda919aa8d9431a4cf
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2024-02-05 23:30:48 -0800

    Time: 57 ms (52.77%), Space: 56.7 MB (16.42%) - LeetHub

--- 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.java
+++ 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.java
@@ -1,15 +1,22 @@
 class Solution {
-    public long[] kthPalindrome(int[] queries, int l) {
-         long[] res = new long[queries.length];
-        int i = 0, half = (l + 1) / 2;
-        long start = (long) Math.pow(10, half - 1), end = (long) Math.pow(10, half) - 1;
-        for (int query : queries) {
-            if (query <= end - start + 1) {
+    public long[] kthPalindrome(int[] queries, int intLength) {
+        int halfLen = (intLength + 1) / 2;
+        int start = (int) Math.pow(10, halfLen - 1);
+        int end = (int) Math.pow(10, halfLen) - 1;
+        
+        long[] result = new long[queries.length];
+        int idx = 0;
+        
+        for(int query : queries) {
+            if(query <= end - start + 1) {
                 String firstHalf = String.valueOf(start + query - 1);
-                String secondHalf = (new StringBuilder(firstHalf)).reverse().toString();
-                res[i++] = Long.parseLong(firstHalf + secondHalf.substring(l % 2));
-            } else res[i++] = -1;
+                String secondHalf = new StringBuilder(firstHalf).reverse().substring(intLength % 2);
+                result[idx++] = Long.parseLong(firstHalf + secondHalf);
+            } else {
+                result[idx++] = -1;
+            }
         }
-        return res;
+        
+        return result;
     }
 }
