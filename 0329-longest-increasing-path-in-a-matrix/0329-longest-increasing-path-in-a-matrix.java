bd908de5a30ebea09092e8b9065a71b88304f2cf
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2024-03-16 19:03:50 -0700

    Time: 8 ms (80.10%), Space: 45.2 MB (21.14%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,35 +1,38 @@
-class Solution {
-    public int longestIncreasingPath(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[][] cache = new int[m][n];
-        int max = 1;
-        
-        for(int i = 0; i< m;i++) {
-            for(int j = 0;j<n;j++) {
-               max = Math.max(max,dfs(matrix, i, j, cache, -1, m , n));
-            }
-        }
-        
-        return max;
-    }
-    
-    public int dfs(int[][] matrix, int i, int j, int[][] cache, int prev, int m, int n) {
-        if(i < 0 || i >= m || j <0 || j >= n || prev >= matrix[i][j]) {
-            return 0;
-        }
-        
+class Solution {  
+    int[][] cache;
+    public int dfs(int[][] matrix, int i, int j, int m, int n) {        
         if(cache[i][j] != 0)
             return cache[i][j];
         
-       int result = 1;
-       int down = 1 + dfs(matrix, i + 1, j, cache, matrix[i][j], m, n);
-       int up = 1 + dfs(matrix, i - 1, j, cache, matrix[i][j], m, n);
-       int right = 1 + dfs(matrix, i, j + 1, cache, matrix[i][j], m, n);
-       int left = 1 + dfs(matrix, i, j - 1, cache, matrix[i][j], m, n);
+        int down = 0;
+        int up = 0;
+        int right = 0;
+        int left = 0;
         
-       result = Math.max(down, Math.max(up, Math.max(left, right)));
-       
-       return cache[i][j] = result;
+        if(i+1<m && matrix[i+1][j] > matrix[i][j])
+            down = 1 + dfs(matrix,i+1,j,m,n);
+        if(i-1>=0 && matrix[i-1][j] > matrix[i][j])
+            up = 1 + dfs(matrix,i-1,j,m,n);
+        if(j+1<n && matrix[i][j+1] > matrix[i][j])
+            right  = 1 + dfs(matrix,i,j+1,m,n);
+        if(j-1>=0 && matrix[i][j-1] > matrix[i][j])
+            left = 1 + dfs(matrix,i,j-1,m,n);
+        
+        return cache[i][j] = Math.max(left, Math.max(right, Math.max(up, down)));
+    }
+    
+    public int longestIncreasingPath(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int maxLength = 0;
+        cache = new int[m][n];
+        
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                maxLength = Math.max(maxLength, dfs(matrix,i,j,m,n));
+            }
+        }
+        
+        return 1 + maxLength;
     }
 }
