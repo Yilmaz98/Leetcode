a20191afd6d2892206c4f35869c56c25a86d4df3
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2024-03-17 19:45:14 -0700

    Time: 45 ms (52.98%), Space: 61.3 MB (30.82%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -34,20 +34,23 @@ class Solution {
         return dp[start] = Math.max(take, notTake);
     }
     
-     int findNext(int idx, List<int[]> jobs) {
-        int lo = idx + 1;
-        int hi = jobs.size() -1;
-        while(lo <= hi) {
-            int mid = lo + (hi - lo)/2;
-            if(jobs.get(mid)[0] >= jobs.get(idx)[1]) {
-                if(jobs.get(mid-1)[0] >= jobs.get(idx)[1])
-                    hi = mid - 1;
-                else 
-                    return mid;
-            } else {
-                lo = mid + 1;
-            }
-        }
-        return -1;
+     int findNext(int cur, List<int[]> jobs) {
+         int low = cur + 1;
+         int high = jobs.size() - 1;
+         int ans = -1;
+         
+         while(low <= high) {
+             int mid = low + (high - low) / 2;
+             
+             if(jobs.get(mid)[0] >= jobs.get(cur)[1]) {
+                 ans = mid;
+                 high = mid - 1;
+             }
+             else {
+                 low = mid + 1;
+             }
+         }
+         
+         return ans;
     }
 }
