0cbc3b377ea6b2fe88652c91d8fa51279340086d
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2024-03-18 03:01:35 -0700

    Time: 26 ms (41.54%), Space: 72.8 MB (95.77%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -5,10 +5,12 @@ class Solution {
         int n = grid2[0].length;
         
         int countsub = 0;
+        boolean[][] visited = new boolean[m][n];
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid2[i][j] == 1) {
-                    if (dfs(grid1, grid2, i, j) == true) {
+                if (grid2[i][j] == 1 && !visited[i][j]) {
+                    if (dfs(grid1, grid2, i, j, visited) == true) {
                         countsub++;
                     }
                 }
@@ -17,22 +19,21 @@ class Solution {
         return countsub;
     }
     
-    private boolean dfs(int[][] grid1, int[][] grid2, int i, int j) {
+    private boolean dfs(int[][] grid1, int[][] grid2, int i, int j, boolean[][] visited) {
         if (i < 0 || j < 0 || i >= grid2.length || j >= grid2[i].length 
-            || grid2[i][j] != 1) {
+            || grid2[i][j] != 1 || visited[i][j]) {
             return true;
         }
         if (grid1[i][j] != grid2[i][j]) {
             return false;
         }
-        grid2[i][j] = -1;
+        visited[i][j] = true;
         
-        boolean left = dfs(grid1, grid2, i - 1, j);
-        boolean right = dfs(grid1, grid2, i + 1, j);
-        boolean up = dfs(grid1, grid2, i, j + 1);
-        boolean down = dfs(grid1, grid2, i, j - 1);
+        boolean left = dfs(grid1, grid2, i - 1, j, visited);
+        boolean right = dfs(grid1, grid2, i + 1, j, visited);
+        boolean up = dfs(grid1, grid2, i, j + 1, visited);
+        boolean down = dfs(grid1, grid2, i, j - 1, visited);
             
         return left && right && up && down;
-
     }
 }
