6641081fcebce58004992738c84c11a3a5b653c5
Mohamed Yilmaz Ibrahim <mohamedyilmaz98@gmail.com>
2024-04-01 13:11:36 -0700

    Time: 25 ms (95.37%), Space: 52 MB (36.53%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,51 +1,44 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        
-        Arrays.sort(nums);
-        recurse(nums, 0, 3, new ArrayList<>(), result, 0);
-        
-        return result;
-    }
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> triplet = new ArrayList<>();
     
-    public void recurse(int[] nums, int start, int k, List<Integer> tempList, List<List<Integer>> result, int sum) {
-        if(k != 2) {
-            for(int i = start;i<nums.length - k + 1;i++) {
-                if(i > start && nums[i] == nums[i-1])
+    public void helper(int k, int[] nums,int start, int target) {
+        if(k!=2) {
+            for(int i= start;i<nums.length - k + 1;i++) {
+                if(i>start && nums[i] == nums[i-1])
                     continue;
-                    tempList.add(nums[i]);
-                    recurse(nums, i + 1, k-1, tempList, result, sum + nums[i]);
-                    tempList.remove(tempList.size() - 1);
+                triplet.add(nums[i]);
+                helper(k-1,nums,i+1, target - nums[i]);
+                triplet.remove(triplet.size() - 1);
             }
             return;
         }
         
-        int low = start;
-        int high = nums.length - 1;
+        int l = start;
+        int r = nums.length - 1;
         
-        while(low < high) {
-            int total = nums[low] + nums[high] + sum;
-            
-            if(total > 0) {
-                high--;
-            } else if(total < 0) {
-                low++;
+        while(l<r) {
+            if(nums[l] + nums[r] > target) {
+                r--;
+            } else if(nums[l] + nums[r] < target) {
+                l++;
             } else {
-                List<Integer> curr = new ArrayList<>();
-                curr.add(nums[low]);
-                curr.add(nums[high]);
-                
-                for(int i=0;i<tempList.size();i++) {
-                    curr.add(tempList.get(i));
+                List<Integer> newList = new ArrayList<>();
+                newList.addAll(triplet);
+                newList.add(nums[l]);
+                newList.add(nums[r]);
+                result.add(newList);
+                l++;
+                while(l<r && nums[l] == nums[l-1]){
+                    l++;
                 }
-                
-                result.add(new ArrayList<>(curr));
+            }
+        }
+    }
     
-                low++;
-                while(low < high && nums[low] == nums[low - 1]) {
-                    low++;
-                }
-            } 
-         }
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        helper(3,nums,0,0);
+        return result;
     }
 }
